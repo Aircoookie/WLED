04684ba59a07737e31734eab97abe1cfd028dabe
Jan <jan.wingefeld@gmail.com>
2020-02-19 22:23:52 +0100

    changed a comment in the ENABLE_DMX line

--- wled00/wled00.ino
+++ wled00/wled00.ino
@@ -24,7 +24,7 @@
 //#define WLED_DISABLE_INFRARED    //there is no pin left for this on ESP8266-01, saves 25kb (!)
 #define WLED_ENABLE_MQTT           //saves 12kb
 #define WLED_ENABLE_ADALIGHT       //saves 500b only
-//#define WLED_ENABLE_DMX            //saves incredible 660b
+//#define WLED_ENABLE_DMX          //uses 3.5kb
 
 #define WLED_DISABLE_FILESYSTEM    //SPIFFS is not used by any WLED feature yet
 //#define WLED_ENABLE_FS_SERVING   //Enable sending html file from SPIFFS before serving progmem version
