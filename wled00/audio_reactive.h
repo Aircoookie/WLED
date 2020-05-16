10fc122025239e6954aa65b225848f732e25c98e
thatdonfc <thatdonfc+git@gmail.com>
2020-05-15 17:07:22 -0700

    Update from THATDONFC

--- wled00/audio_reactive.h
+++ wled00/audio_reactive.h
@@ -5,6 +5,8 @@
  * bytes 2400+ are currently ununsed, but might be used for future wled features
  */
 
+#include "wled.h"
+
 #ifndef ESP8266
   TaskHandle_t FFT_Task;
 #endif
@@ -14,59 +16,35 @@
 #ifdef ESP8266
   #define MIC_PIN   A0
 #else
-  #define MIC_PIN   36 //  Changed to directly naming pin since ESP32 has multiple ADCs 8266: A0  ESP32: 36(ADC1_0) Analog port for microphone
-#ifndef LED_BUILTIN
-  // Set LED_BUILTIN if it is not defined by Arduino framework
+  #define MIC_PIN   36    // Changed to direct pin name since ESP32 has multiple ADCs 8266: A0  ESP32: 36(ADC1_0) Analog port for microphone
+#ifndef LED_BUILTIN       // Set LED_BUILTIN if it is not defined by Arduino framework
   #define LED_BUILTIN 3
 #endif
 #endif
 
 // As defined in wled00.h
-// byte soundSquelch = 10;                                    // default squelch value for volume reactive routines
-// uint16_t noiseFloor = 100;                                 // default squelch value for FFT reactive routines
+// byte soundSquelch = 10;                          // default squelch value for volume reactive routines
+// uint16_t noiseFloor = 100;                       // default squelch value for FFT reactive routines
 
-int micIn;                                                    // Current sample starts with negative values and large values, which is why it's 16 bit signed
-int sample;                                                   // Current sample
-float sampleAvg = 0;                                          // Smoothed Average
-float micLev = 0;                                             // Used to convert returned value to have '0' as minimum. A leveller
-uint8_t maxVol = 11;                                          // Reasonable value for constant volume for 'peak detector', as it won't always trigger
-bool samplePeak = 0;                                          // Boolean flag for peak. Responding routine must reset this flag
+int micIn;                                          // Current sample starts with negative values and large values, which is why it's 16 bit signed
+int sample;                                         // Current sample
+float sampleAvg = 0;                                // Smoothed Average
+float micLev = 0;                                   // Used to convert returned value to have '0' as minimum. A leveller
+uint8_t maxVol = 11;                                // Reasonable value for constant volume for 'peak detector', as it won't always trigger
+bool samplePeak = 0;                                // Boolean flag for peak. Responding routine must reset this flag
 
-int sampleAgc;                                                // Our AGC sample
-float multAgc;                                                // sample * multAgc = sampleAgc. Our multiplier
-uint8_t targetAgc = 60;                                       // This is our setPoint at 20% of max for the adjusted output
+int sampleAgc;                                      // Our AGC sample
+float multAgc;                                      // sample * multAgc = sampleAgc. Our multiplier
+uint8_t targetAgc = 60;                             // This is our setPoint at 20% of max for the adjusted output
 
 long lastTime = 0;
-int delayMs = 10;                                             // I don't want to sample too often and overload WLED.
-double beat = 0;                                              // beat Detection
+int delayMs = 10;                                   // I don't want to sample too often and overload WLED.
+double beat = 0;                                    // beat Detection
 
-uint16_t micData;
+uint16_t micData;                                   // Analog input for FFT
+uint16_t lastSample;                                // last audio noise sample
 
-uint8_t myVals[32];                                           // Used to store a pile of samples as WLED frame rate and WLED sample rate are not synchronized
-
-
-#ifndef ESP8266
-  #include "arduinoFFT.h"
-  //#include "movingAvg.h"
-
-  arduinoFFT FFT = arduinoFFT();                              // Create FFT object
-
-  const uint16_t samples = 512;                               // This value MUST ALWAYS be a power of 2
-  const double samplingFrequency = 10240;                     // Sampling frequency
-
-  unsigned int sampling_period_us;
-  unsigned long microseconds;
-
-  /*
-  These are the input and output vectors
-  Input vectors receive computed results from FFT
-  */
-  double fftBin[samples];
-  double vReal[samples];
-  double vImag[samples];
-#endif
-
-uint16_t lastSample;                                          // last audio noise sample
+uint8_t myVals[32];                                 // Used to store a pile of samples as WLED frame rate and WLED sample rate are not synchronized
 
 void getSample() {
   static long peakTime;
@@ -130,101 +108,108 @@ void agcAvg() {                                                       // A simpl
 
 } // agcAvg()
 
+////////////////////
+// Begin FFT Code //
+////////////////////
+
 #ifndef ESP8266
+  #include "arduinoFFT.h"
+  //#include "movingAvg.h"
+  const uint16_t samples = 512;                     // This value MUST ALWAYS be a power of 2
+  const double samplingFrequency = 10240;           // Sampling frequency in Hz
+  unsigned int sampling_period_us;
+  unsigned long microseconds;
 
-double fftResult[16];
-uint16_t mAvg = 0;
+  double FFT_MajorPeak = 0;
+  double FFT_Magnitude = 0;
+  uint16_t mAvg = 0;
 
-double fftAdd( int from, int to) {
-  int i = from;
-  double result = 0;
+  /*
+  These are the input and output vectors
+  Input vectors receive computed results from FFT
+  */
+  double vReal[samples];
+  double vImag[samples];
+  double fftBin[samples];
+  double fftResult[16];
 
-  while ( i <= to) {
-    result += fftBin[i++];
+  // Create FFT object
+  arduinoFFT FFT = arduinoFFT( vReal, vImag, samples, samplingFrequency );
+
+  double fftAdd( int from, int to) {
+    int i = from;
+    double result = 0;
+    while ( i <= to) {
+      result += fftBin[i++];
+    }
+    return result;
   }
 
-  return result;
-}
+  // FFT main code
+  void FFTcode( void * parameter) {
+    double beatSample = 0;
+    double envelope = 0;
+    uint16_t rawMicData = 0;
 
-uint16_t FFT_MajorPeak = 0;
-double FFT_mpX = 0;
-double FFT_mpV = 0;
+    for(;;) {
+      delay(1);           // DO NOT DELETE THIS LINE! It is needed to give the IDLE(0) task enough time and to keep the watchdog happy.
+      microseconds = micros();
+      extern double volume;
 
-// FFT main code
-void FFTcode( void * parameter) {
-  double sum, mean = 0;
-  double beatSample = 0;
-  double envelope = 0;
-  uint16_t rawMicData = 0;
+      for(int i=0; i<samples; i++) {
+        micData = analogRead(MIC_PIN);                        // Analog Read
+        rawMicData = micData >> 2;                            // ESP32 has 12 bit ADC
+        vReal[i] = micData;                                   // Store Mic Data in an array
+        vImag[i] = 0;
 
-  for(;;) {
-    delay(1);           // DO NOT DELETE THIS LINE! It is needed to give the IDLE(0) task enough time and to keep the watchdog happy.
-    microseconds = micros();
-    extern double volume;
+  //      rawMicData = rawMicData - mAvg;                     // center
+  //      beatSample = bassFilter(rawMicData);
+  //      if (beatSample < 0) beatSample =-beatSample;        // abs
+  //      envelope = envelopeFilter(beatSample);
 
-    for(int i=0; i<samples; i++)
-    {
-      micData = analogRead(MIC_PIN);
-      rawMicData = micData >> 2;
-      vReal[i] = micData;
-      vImag[i] = 0;
-
-//      rawMicData = rawMicData - mAvg;                     // center
-//      beatSample = bassFilter(rawMicData);
-//      if (beatSample < 0) beatSample =-beatSample;        // abs
-//      envelope = envelopeFilter(beatSample);
-
-      while(micros() - microseconds < sampling_period_us){
-        //empty loop
-        }
+        while(micros() - microseconds < sampling_period_us){
+          //empty loop
+          }
         microseconds += sampling_period_us;
-    }
+      }
 
-//  beat = beatFilter(envelope);
-//  if (beat > 50000) digitalWrite(LED_BUILTIN, HIGH); else digitalWrite(LED_BUILTIN, LOW);
+  //  beat = beatFilter(envelope);
+  //  if (beat > 50000) digitalWrite(LED_BUILTIN, HIGH); else digitalWrite(LED_BUILTIN, LOW);
 
+      FFT.Windowing( FFT_WIN_TYP_HAMMING, FFT_FORWARD );    // Weigh data
+      FFT.Compute( FFT_FORWARD );                           // Compute FFT
+      FFT.ComplexToMagnitude();                             // Compute magnitudes
 
-    FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);   // Weigh data
-    FFT.Compute(vReal, vImag, samples, FFT_FORWARD);                   // Compute FFT
-    FFT.ComplexToMagnitude(vReal, vImag, samples);                     // Compute magnitudes
+      /*
+       * vReal[8 .. 511] contain useful data, each a 20Hz interval (140Hz - 10220Hz).
+       * There could be interesting data at [2 .. 7] but chances are there are too many artifacts
+       */
+      FFT.MajorPeak(&FFT_MajorPeak, &FFT_Magnitude);        // let the effects know which freq was most dominant
 
-    sum = 0;
-    // Normalize bins
-    for ( int i = 1; i < samples; i++) {
-      sum += vReal[i];
-    }
-    mean = sum / samples;
-    for ( int i = 0; i < samples; i++) {
-      vReal[i] -= mean;
-      if (vReal[i] < 0) vReal[i] = 0;
-    }
+      if (FFT_Magnitude > 65535) FFT_Magnitude = 0;         // FFT_Magnitude just skyrockets when the volume is quiet. Very strange.
 
-    // vReal[8 .. 511] contain useful data, each a 20Hz interval (140Hz - 10220Hz).
-    // There could be interesting data at [2 .. 7] but chances are there are too many artifacts.
+      for (int i = 0; i < samples; i++) fftBin[i] = vReal[i];   // export FFT field
 
-    FFT.MajorPeak(vReal, samples, samplingFrequency, &FFT_mpX, &FFT_mpV);  // Let the effects know which freq/volume was most dominant.
-    FFT_MajorPeak = FFT_mpX;                  // Current effects use this value.
-    if (FFT_mpV > 65535) FFT_mpV = 0;         // FFT_mpV just skyrockets when the volume is quiet. Very strange.
-
-    for (int i = 0; i < samples; i++) fftBin[i] = vReal[i];       // export FFT field
-
-    // Create an array of 16 bins which roughly represent values the human ear can determine as different frequency bands (fftBins[0..6] are already zero'd)
-    fftResult[0] = fftAdd(7,11) * 0.8;
-    fftResult[1] = fftAdd(12,16);
-    fftResult[2] = fftAdd(17,21);
-    fftResult[3] = fftAdd(22, 30);
-    fftResult[4] = fftAdd(31, 39);
-    fftResult[5] = fftAdd(40, 48);
-    fftResult[6] = fftAdd(49, 61);
-    fftResult[7] = fftAdd(62, 78);
-    fftResult[8] = fftAdd(79, 99);
-    fftResult[9] = fftAdd(100, 124);
-    fftResult[10] = fftAdd(125, 157);
-    fftResult[11] = fftAdd(158, 198);
-    fftResult[12] = fftAdd(199, 247);
-    fftResult[13] = fftAdd(248, 312);
-    fftResult[14] = fftAdd(313, 393);
-    fftResult[15] = fftAdd(394, 470);
+      /*
+       * Create an array of 16 bins which roughly represent values the human ear
+       * can determine as different frequency bands (fftBins[0..6] are already zero'd)
+       */
+      fftResult[0] = fftAdd(7,11) * 0.8;
+      fftResult[1] = fftAdd(12,16);
+      fftResult[2] = fftAdd(17,21);
+      fftResult[3] = fftAdd(22, 30);
+      fftResult[4] = fftAdd(31, 39);
+      fftResult[5] = fftAdd(40, 48);
+      fftResult[6] = fftAdd(49, 61);
+      fftResult[7] = fftAdd(62, 78);
+      fftResult[8] = fftAdd(79, 99);
+      fftResult[9] = fftAdd(100, 124);
+      fftResult[10] = fftAdd(125, 157);
+      fftResult[11] = fftAdd(158, 198);
+      fftResult[12] = fftAdd(199, 247);
+      fftResult[13] = fftAdd(248, 312);
+      fftResult[14] = fftAdd(313, 393);
+      fftResult[15] = fftAdd(394, 470);
   }
 }
 
