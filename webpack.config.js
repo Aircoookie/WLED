7767ea1d3bce2c22e584f232b7812ac2ebd0dcc7
Phil Bolduc <philbolduc@gmail.com>
2020-09-01 19:14:06 -0700

    Fix settings causing html attibutes to be removed, change to use terse plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,45 @@
 var path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin"); 
-const InlineSourceWebpackPlugin = require('inline-source-webpack-plugin'); 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const InlineSourceWebpackPlugin = require('inline-source-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = { 
- 
-    // Path to your entry point. From this file Webpack will begin his work 
-    entry: './wled00/data/index.js', 
-   
-    // Path and filename of your result bundle. 
-    // Webpack will bundle all JavaScript into this file 
-    output: { 
-      path: path.resolve(__dirname, 'wled00/data/dist'), 
-      filename: 'index.js'
-    }, 
-   
-    mode: 'development', 
-   
-    module: { 
-      rules: [ 
-        { 
-          test: /\.js$/, 
-          exclude: /(node_modules)/, 
-          use: { 
-            loader: 'babel-loader', 
-            options: { 
-              presets: ['@babel/preset-env'] 
-            } 
-          } 
-        } 
-      ] 
-    },
-    optimization: { 
-      minimize: true, 
-      minimizer: [new UglifyJsPlugin({ 
-        sourceMap: false, 
-      })] 
-    }, 
-    plugins: [ 
-      new HtmlWebpackPlugin({ 
-        inlineSource: '.(js|css)$', // embed all javascript and css inline, 
-        template: path.resolve(__dirname, "wled00/data", "index.htm"), 
-        hash: false 
-      }), 
-      new InlineSourceWebpackPlugin({ 
-        compress: true, 
-        rootpath: './wled00/data/dist', 
-        noAssetMatch: 'warn' 
-      }) 
-    ]
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin his work
+  entry: './wled00/data/index.js',
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'wled00/data/dist'),
+    filename: 'index.js'
+  },
+  mode: 'development',
+  module: {
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      extractComments: 'some'
+    })]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inlineSource: '.(js|css)$', // embed all javascript and css inline,
+      template: path.resolve(__dirname, "wled00/data", "index.htm"),
+      hash: false,
+      minify: {
+        removeRedundantAttributes: false,
+        removeAttributeQuotes: false,
+        removeComments: true,
+        sortAttributes: true,
+        collapseWhitespace: true
+      }
+    }),
+    new InlineSourceWebpackPlugin({
+      compress: true,
+      rootpath: './wled00/data/dist',
+      noAssetMatch: 'warn'
+    })
+  ]
 };
