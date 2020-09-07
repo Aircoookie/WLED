a547ec1c874fd61954e996299a9616adffd5fb9d
Phil Bolduc <philbolduc@gmail.com>
2020-09-06 22:45:58 -0700

    Change Temperature usermod to use async reading to avoid blocking

--- usermods/Temperature/usermod_temperature.h
+++ usermods/Temperature/usermod_temperature.h
@@ -22,27 +22,58 @@ class UsermodTemperature : public Usermod {
   private:
     //set last reading as "40 sec before boot", so first reading is taken after 20 sec
     unsigned long lastMeasurement = UINT32_MAX - 40000;
+    // last time requestTemperatures was called
+    unsigned long lastTempRequest;
+    bool waitingForConversion = false;
     float temperature = 0.0f;
+
+    void requestTemperatures() {
+      sensor.requestTemperatures();
+      waitingForConversion = true;
+      lastTempRequest = millis();
+    }
+
+    /**
+     * Calculates the elapsed milliseconds since the specified time.
+     * Handles the case where millis() rolls over after roughly 49.7 days
+     */
+    unsigned long elapsed(unsigned long since) {
+      unsigned long now = millis();
+
+      // no rollover?
+      if (since <= now) {
+        return now - since;
+      }
+
+      // rollover in millis - happens after roughly 49.7 days
+      return (UINT32_MAX - since) + now;
+    }
+
   public:
     void getReading() {
-      sensor.requestTemperatures();
       #ifdef TEMP_CELSIUS
       temperature = sensor.getTempCByIndex(0);
       #else
       temperature = sensor.getTempFByIndex(0);
       #endif
+      waitingForConversion = false;
     }
 
     void setup() {
       sensor.begin();
       sensor.setResolution(9);
+      sensor.setWaitForConversion(false); // do not block waiting for reading
     }
 
     void loop() {
-      if (millis() - lastMeasurement > MEASUREMENT_INTERVAL)
+      if (!waitingForConversion && elapsed(lastMeasurement) > MEASUREMENT_INTERVAL)
       {
-        getReading();
-
+        requestTemperatures();
+      }
+      else if (waitingForConversion && elapsed(lastTempRequest) > 94 /* 93.75ms per the datasheet */)
+      {
+        getReading(); // this will reset waitingForConversion = false
+ 
         if (WLED_MQTT_CONNECTED) {
           char subuf[38];
           strcpy(subuf, mqttDeviceTopic);
