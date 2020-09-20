1313a44ba373c6621809e825358d9dee7d5ca89d
Peter <pfeerick@users.noreply.github.com>
2020-09-21 00:00:24 +1000

    ESP8266: Allow auto modem sleep if power off and sleep permitted (#1184)
    * Allow modem sleep if power off and sleep permitted
    
    * Only add auto modem sleep handling for ESP8266
    
    * Add comment and remove redundant condition
    
    Co-authored-by: Aircoookie <cschwinne@gmail.com>

--- wled00/wled.cpp
+++ wled00/wled.cpp
@@ -84,10 +84,14 @@ void WLED::loop()
 
     handleHue();
     handleBlynk();
-
     yield();
+
     if (!offMode)
       strip.service();
+#ifdef ESP8266
+    else if (!noWifiSleep)
+      delay(1); //required to make sure ESP enters modem sleep (see #1184)
+#endif
   }
   yield();
 #ifdef ESP8266
