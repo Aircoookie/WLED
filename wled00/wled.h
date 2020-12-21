f7893d4e4b62b46c0034d36e8d17754be80e8ae5
cschwinne <dev.aircoookie@gmail.com>
2020-12-21 19:48:21 +0100

    Fixed JShint warnings and minify HTML

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2012190
+#define VERSION 2012210
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
