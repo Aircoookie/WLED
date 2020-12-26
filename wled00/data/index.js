8f52a5ee578b5610adca596c979027c9ac84f024
cheffpj2 <patrick.j.mcdonough@gmail.com>
2020-12-26 03:20:25 -0800

    runs w/ multi and temp on my preasmbld QL-DQuad

--- wled00/data/index.js
+++ wled00/data/index.js
@@ -470,10 +470,12 @@ function populateInfo(i)
 	cn += `v${i.ver} "${vcn}"<br><br><table class="infot">
 	${urows}
 	${inforow("Build",i.vid)}
+	${inforow("Custom","QL-DigQuad-Pre-v0.1")}
 	${inforow("Signal strength",i.wifi.signal +"% ("+ i.wifi.rssi, " dBm)")}
 	${inforow("Uptime",getRuntimeStr(i.uptime))}
 	${inforow("Free heap",heap," kB")}
 	${inforow("Estimated current",pwru)}
+	${inforow("Temperature",i.u.Temperature[0] + i.u.Temperature[1])}
 	${inforow("MAC address",i.mac)}
 	${inforow("Filesystem",i.fs.u + "/" + i.fs.t + " kB (" +Math.round(i.fs.u*100/i.fs.t) + "%)")}
 	${inforow("Environment",i.arch + " " + i.core + " (" + i.lwip + ")")}
