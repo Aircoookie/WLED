74eca46cd406058620397844a63e8acc423a76bb
Rolf <>
2021-02-02 23:39:31 +0100

    Settings in flash, info and API

--- usermods/PIR_staircase/PIR_staircase.h
+++ usermods/PIR_staircase/PIR_staircase.h
@@ -2,6 +2,7 @@
 
 #include "math.h"
 #include "wled.h"
+#define USERMOD_ID_PIR_STAIRCASE 1011
 
 /*
  * Usermod for detecting people entering/leaving a staircase and switching the
@@ -9,16 +10,14 @@
  */
 
 // Please change the pin numbering to match your board.
-const int topPIR_PIN    = D6;
-const int bottomPIR_PIN = D5;
+const int topPIR_PIN = D7;
+const int bottomPIR_PIN = D6;
 
-#define USERMOD_ID_PIR_STAIRCASE 1011
-
-// Time between switching on/off each segment
-const unsigned long SEGMENT_DELAY_MS = 150;
+// Time between switching on/off each segment, stored in config
+unsigned long segment_delay_ms = 150;
 
 // The time for the light to stay on
-const unsigned long ON_TIME = 5 * 1000;
+unsigned long on_time_ms = 5 * 1000;
 
 // Time between checking of the PIRs
 const int scanDelay = 50;
@@ -60,6 +59,8 @@ class PIR_staircase : public Usermod {
   byte maxSegmentId = 1;
   byte mainSegmentId = 0;
 
+  bool saveState = false;
+
   void updateSegments() {
     mainSegmentId = strip.getMainSegmentId();
     WS2812FX::Segment mainsegment = strip.getSegment(mainSegmentId);
@@ -91,6 +92,12 @@ class PIR_staircase : public Usermod {
       bool bottomPIR = digitalRead(bottomPIR_PIN) == HIGH;
       bool topPIR = digitalRead(topPIR_PIN) == HIGH;
 
+      // Serial.print(millis(),DEC);
+      // Serial.print(" ");
+      // Serial.print(bottomPIR);
+      // Serial.print(" ");
+      // Serial.println(topPIR);
+
       if (bottomPIR != topPIR) {
         lastSwitchTime = millis();
 
@@ -99,6 +106,12 @@ class PIR_staircase : public Usermod {
         } else {
           // If the bottom PIR triggered, we need to swipe up, ON
           swipe = bottomPIR;
+
+          if (swipe) {
+            Serial.println("ON -> Swipe up.");
+          } else {
+            Serial.println("ON -> Swipe down.");
+          }
 
           if (onIndex == offIndex) {
             // Position the indices for a correct on-swipe
@@ -116,15 +129,21 @@ class PIR_staircase : public Usermod {
   }
 
   void autoPowerOff() {
-    if (on && (millis() - lastSwitchTime) > ON_TIME) {
+    if (on && (millis() - lastSwitchTime) > on_time_ms) {
       // Swipe OFF in the direction of the last PIR detection
       swipe = lastPIR;
       on = false;
+
+      if (swipe) {
+        Serial.println("OFF -> Swipe up.");
+      } else {
+        Serial.println("OFF -> Swipe down.");
+      }
     }
   }
 
   void updateSwipe() {
-    if ((millis() - lastTime) > SEGMENT_DELAY_MS) {
+    if ((millis() - lastTime) > segment_delay_ms) {
       lastTime = millis();
 
       byte oldOnIndex = onIndex;
@@ -149,6 +168,12 @@ class PIR_staircase : public Usermod {
  public:
   void setup() {
     Serial.println("PIR Staircase enabled.");
+    Serial.print("Delay between steps: ");
+    Serial.print(segment_delay_ms, DEC);
+    Serial.print(" milliseconds.\nStairs switch off after: ");
+    Serial.print(on_time_ms / 1000, DEC);
+    Serial.println(" seconds.");
+
     pinMode(bottomPIR_PIN, INPUT);
     pinMode(topPIR_PIN, INPUT);
   }
@@ -157,7 +182,102 @@ class PIR_staircase : public Usermod {
     checkPIRs();
     autoPowerOff();
     updateSwipe();
+
+    // Write changed settings from the json api into flash now
+    if (saveState) {
+      serializeConfig();
+      saveState = false;
+    }
   }
 
   uint16_t getId() { return USERMOD_ID_PIR_STAIRCASE; }
+
+  /*
+   * Shows configuration settings to the json API. This object looks like:
+   *
+   * "staircase" : {
+   *   "segment-delay-ms" : 150,
+   *   "on-time-s" : 5
+   * }
+   *
+   */
+  void addToJsonState(JsonObject& root) {
+    JsonObject staircase = root["staircase"];
+    if (staircase.isNull()) {
+      staircase = root.createNestedObject("staircase");
+    }
+    staircase["segment-delay-ms"] = segment_delay_ms;
+    staircase["on-time-s"] = on_time_ms / 1000;
+  }
+
+  /*
+   * Reads configuration settings from the json API.
+   * See void addToJsonState(JsonObject& root)
+   */
+  void readFromJsonState(JsonObject& root) {
+    JsonObject staircase = root["staircase"];
+    unsigned long s = staircase["segment-delay-ms"] | 150;
+    unsigned long o = (staircase["on-time-s"] | 5) * 1000;
+
+    if (s != segment_delay_ms) {
+      segment_delay_ms = s;
+      saveState = true;
+    }
+
+    if (o != on_time_ms / 1000) {
+      on_time_ms = s * 1000;
+      saveState = true;
+    }
+
+    if (saveState) {
+      Serial.println("Staircase settings changed by API call.");
+    }
+  }
+
+  /*
+   * Writes the configuration to internal flash memory.
+   */
+  void addToConfig(JsonObject& root) {
+    JsonObject staircase = root["staircase"];
+    if (staircase.isNull()) {
+      staircase = root.createNestedObject("staircase");
+    }
+
+    staircase["segment-delay-ms"] = segment_delay_ms;
+    staircase["on-time-s"] = on_time_ms / 1000;
+    Serial.println("Staircase config saved.");
+  }
+
+  /*
+   * Reads the configuration to internal flash memory before setup() is called.
+   */
+  void readFromConfig(JsonObject& root) {
+    JsonObject staircase = root["staircase"];
+    segment_delay_ms = staircase["segment-delay-ms"] | 150;
+    on_time_ms = (staircase["on-time-s"] | 5) * 1000;
+    Serial.println("Staircase config loaded.");
+  }
+
+  /*
+   * Shows the delay between steps and power-off time in the "info"
+   * tab of the web-UI.
+   */
+  void addToJsonInfo(JsonObject& root) {
+    int reading = 20;
+    // this code adds "u":{"Light":[20," lux"]} to the info object
+    JsonObject staircase = root["u"];
+    if (staircase.isNull()) {
+      staircase = root.createNestedObject("u");
+    }
+
+    JsonArray segmentDelay =
+        staircase.createNestedArray("Delay between stairs");  // name
+    segmentDelay.add(segment_delay_ms);                       // value
+    segmentDelay.add(" milliseconds");                        // unit
+
+    JsonArray onTime =
+        staircase.createNestedArray("Power-off stairs after");  // name
+    onTime.add(on_time_ms / 1000);                              // value
+    onTime.add(" seconds");                                     // unit
+  }
 };
