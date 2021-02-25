10746ae98c0f979b16dd39391dfaba96cdb76317
Andrew Tuline <atuline@gmail.com>
2021-02-25 13:02:17 -0800

    Fix Gravcenter, now that I'd fixed the others.

--- wled00/FX.cpp
+++ wled00/FX.cpp
@@ -4255,23 +4255,23 @@ uint16_t WS2812FX::mode_gravcenter(void) {                // Gravcenter. By Andr
 
   sampleAvg = sampleAvg * SEGMENT.intensity / 255;
 
-  int tempsamp = constrain(sampleAvg*2,0,SEGLEN/2-1);     // Keep the sample from overflowing.
+  int tempsamp = constrain(sampleAvg*2,0,SEGLEN/2);     // Keep the sample from overflowing.
   uint8_t gravity = 8 - SEGMENT.speed/32;
 
   for (int i=0; i<tempsamp; i++) {
     uint8_t index = inoise8(i*sampleAvg+millis(), 5000+i*sampleAvg);
     setPixelColor(i+SEGLEN/2, color_blend(SEGCOLOR(1), color_from_palette(index, false, PALETTE_SOLID_WRAP, 0), sampleAvg*8));
-    setPixelColor(SEGLEN/2-i, color_blend(SEGCOLOR(1), color_from_palette(index, false, PALETTE_SOLID_WRAP, 0), sampleAvg*8));
+    setPixelColor(SEGLEN/2-i-1, color_blend(SEGCOLOR(1), color_from_palette(index, false, PALETTE_SOLID_WRAP, 0), sampleAvg*8));
   }
 
   if (tempsamp >= topLED)
-    topLED = tempsamp;
+    topLED = tempsamp-1;
   else if (gravityCounter % gravity == 0)
     topLED--;
 
-  if (topLED > 0) {
+  if (topLED >= 0) {
     setPixelColor(topLED+SEGLEN/2, color_blend(SEGCOLOR(1), color_from_palette(millis(), false, PALETTE_SOLID_WRAP, 0), 255));
-    setPixelColor(SEGLEN/2-topLED, color_blend(SEGCOLOR(1), color_from_palette(millis(), false, PALETTE_SOLID_WRAP, 0), 255));
+    setPixelColor(SEGLEN/2-1-topLED, color_blend(SEGCOLOR(1), color_from_palette(millis(), false, PALETTE_SOLID_WRAP, 0), 255));
   }
   gravityCounter = (gravityCounter + 1) % gravity;
 
@@ -4310,7 +4310,7 @@ uint16_t WS2812FX::mode_gravcentric(void) {               // Gravcenter. By Andr
 
   if (topLED >= 0) {
     setPixelColor(topLED+SEGLEN/2, CRGB::Gray);
-    setPixelColor(SEGLEN/2-1-topLED, CRGB::Gray);
+    setPixelColor(SEGLEN/2-1-topLED, CRGB::Red);
   }
   gravityCounter = (gravityCounter + 1) % gravity;
 
