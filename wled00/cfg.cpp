6ccdec6ca49a2840c016f1806918c9eedcefd8a1
Ahmed Shehata <4982099+ahmed-shehata@users.noreply.github.com>
2021-04-17 03:35:00 +0100

    added save/load config to fs

--- wled00/cfg.cpp
+++ wled00/cfg.cpp
@@ -222,6 +222,9 @@ void deserializeConfig() {
   JsonObject if_sync = interfaces[F("sync")];
   CJSON(udpPort, if_sync[F("port0")]); // 21324
   CJSON(udpPort2, if_sync[F("port1")]); // 65506
+  CJSON(enableHyperionColorCorrection, if_sync[F("correction")]);
+  CJSON(hyperionHSVSaturation, if_sync[F("hsvsaturation")]);
+  CJSON(hyperionHSVValue, if_sync[F("hsvvalue")]);
 
   JsonObject if_sync_recv = if_sync["recv"];
   CJSON(receiveNotificationBrightness, if_sync_recv["bri"]);
@@ -542,6 +545,9 @@ void serializeConfig() {
   JsonObject if_sync = interfaces.createNestedObject("sync");
   if_sync[F("port0")] = udpPort;
   if_sync[F("port1")] = udpPort2;
+  if_sync[F("correction")] = enableHyperionColorCorrection;
+  if_sync[F("hsvsaturation")] = hyperionHSVSaturation;
+  if_sync[F("hsvvalue")] = hyperionHSVValue;
 
   JsonObject if_sync_recv = if_sync.createNestedObject("recv");
   if_sync_recv["bri"] = receiveNotificationBrightness;
