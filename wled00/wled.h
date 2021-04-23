d6338d7b110a13e81d069d9115f328181778c897
Blaz Kristan <blaz@kristan-sp.si>
2021-04-23 17:21:45 +0200

    Changed codename to Ryujin. Added blinking colon on display usermod.

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2104241
+#define VERSION 2104242
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
@@ -179,7 +179,7 @@
 
 // Global Variable definitions
 WLED_GLOBAL char versionString[] _INIT(TOSTRING(WLED_VERSION));
-#define WLED_CODENAME "Hikari"
+#define WLED_CODENAME "Ryujin"
 
 // AP and OTA default passwords (for maximum security change them!)
 WLED_GLOBAL char apPass[65]  _INIT(DEFAULT_AP_PASS);
