d6dad02e305e9a2b5372494b1d8a4b24760c3a77
thatdonfc <thatdonfc+git@gmail.com>
2021-05-12 09:10:07 -0700

    Grammar fixes

--- wled00/html_settings.h
+++ wled00/html_settings.h
@@ -87,7 +87,7 @@ type="number" min="1" max="1500" oninput="UI()"> x <input name="MXH"
 type="number" min="1" max="1500" oninput="UI()"><br><div id="2dwarning" 
 style="color:orange;display:inline">
 &#9888; width x height should match LED count!<br></div>Serpentine: <input 
-type="checkbox" name="MXWHS" checked="checked"> (how leds are layed out)<br><br>
+type="checkbox" name="MXWHS" checked="checked"> (how leds are laid out)<br><br>
 Enable automatic brightness limiter: <input type="checkbox" name="ABen" 
 onchange="enABL()" id="able"><br><div id="abl">Maximum Current: <input 
 name="MA" type="number" min="250" max="65000" oninput="UI()" required> mA<br>
