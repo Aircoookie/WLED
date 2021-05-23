11c7d586d9f53aee0ef8725cb88e1d827c556708
Blaz Kristan <blaz@kristan-sp.si>
2021-05-23 18:22:26 +0200

    Bugfix. - atan_t(x) for x>1 - incorrect switch type and missing analog

--- wled00/wled_math.h
+++ wled00/wled_math.h
@@ -58,6 +58,7 @@ float asin_t(float x) {
   return HALF_PI - acos_t(x);
 }
 
+/*
 //https://stackoverflow.com/a/42542593
 #define A 0.0776509570923569
 #define B -0.287434475393028
@@ -67,6 +68,42 @@ float atan_t(float x) {
   if (x>1) return atan(x);
   float xx = x * x;
   return ((A*xx + B)*xx + C)*x;
+}
+*/
+//https://stackoverflow.com/a/42542593
+#define A 0.0776509570923569
+#define B -0.287434475393028
+#define C ((HALF_PI/2) - A - B)
+
+//polynominal factors for approximation between 1 and 5
+#define C0 0.089494f
+#define C1 0.974207f
+#define C2 -0.326175f
+#define C3 0.05375f
+#define C4 -0.003445f
+
+float atan_t(float x) {
+  bool neg = (x < 0);
+  #ifdef WLED_DEBUG_MATH
+  float xinput = x;
+  #endif
+  x = std::abs(x);
+  float res;
+  if (x > 5.0f) { //atan(x) converges to pi/2 - (1/x) for large values
+    res = HALF_PI - (1.0f/x);
+  }
+  else if (x > 1.0f) { //1 < x < 5
+    float xx = x * x;
+    res = (C4*xx*xx)+(C3*xx*x)+(C2*xx)+(C1*x)+C0;
+  } else { //this approximation is only for x <= 1
+    float xx = x * x;
+    res = ((A*xx + B)*xx + C)*x;
+  }
+  if (neg) res = -res;
+  #ifdef WLED_DEBUG_MATH
+  Serial.printf("atan,%f,%f,%f\n",xinput,res,atan(xinput));
+  #endif
+  return res;
 }
 
 float floor_t(float x) {
