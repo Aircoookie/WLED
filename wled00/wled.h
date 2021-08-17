b93c47fa60b991c90a88e2bc8900aa91607ace1f
Blaz Kristan <blaz@kristan-sp.si>
2021-08-17 16:28:12 +0200

    Option to disable Loxone.

--- wled00/wled.h
+++ wled00/wled.h
@@ -33,7 +33,9 @@
 #endif
 //#define WLED_ENABLE_ADALIGHT     // saves 500b only (uses GPIO3 (RX) for serial)
 //#define WLED_ENABLE_DMX          // uses 3.5kb (use LEDPIN other than 2)
-#define WLED_ENABLE_LOXONE         // uses 1.2kb
+#ifndef WLED_DISABLE_LOXONE
+  #define WLED_ENABLE_LOXONE       // uses 1.2kb
+#endif
 #ifndef WLED_DISABLE_WEBSOCKETS
   #define WLED_ENABLE_WEBSOCKETS
 #endif
