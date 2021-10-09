afe449c0f3547487a8445886d422cf6fd4e14fb4
Phil Bolduc <philbolduc@gmail.com>
2021-10-09 10:37:55 -0700

    More POC to dynamically add content, minimal DOM lib based on chibi

--- wled00/data/cfg.js
+++ wled00/data/cfg.js
@@ -1,141 +1,35 @@
-
 import { lang } from './cfg_lang.js'
-
-// minimal version of chibi - todo convert to module exporting '$' and remove from this file
-(function () {
-	'use strict';
-	
-	var readyfn = [],
-		loadedfn = [],
-		domready = false,
-		pageloaded = false,		
-		jsonpcount = 0,
-		d = document,
-		w = window;
-		
-	// Fire any function calls on ready event
-	function fireReady() {
-		var i;
-		domready = true;
-		for (i = 0; i < readyfn.length; i += 1) {
-			readyfn[i]();
-		}
-		readyfn = [];
-	}
-
-	// Fire any function calls on loaded event
-	function fireLoaded() {
-		var i;
-		pageloaded = true;
-		for (i = 0; i < loadedfn.length; i += 1) {
-			loadedfn[i]();
-		}
-		loadedfn = [];
-	}
-
-	// Check DOM ready, page loaded
-	if (d.addEventListener) {
-		// Standards
-		d.addEventListener('DOMContentLoaded', fireReady, false);
-		w.addEventListener('load', fireLoaded, false);
-	}	
-
-	// Loop through node array
-	function nodeLoop(fn, nodes) {
-		var i;
-		// Good idea to walk up the DOM
-		for (i = nodes.length - 1; i >= 0; i -= 1) {
-			fn(nodes[i]);
-		}
-	}		
-
-	function wled(selector) {
-		var cb, nodes = [], json = false, nodelist, i;
-
-		if (selector) {
-
-			// Element node, would prefer to use (selector instanceof HTMLElement) but no IE support
-			if (selector.nodeType && selector.nodeType === 1) {
-				nodes = [selector]; // return element as node list
-			} else if (typeof selector === 'object') {
-				// JSON, document object or node list, would prefer to use (selector instanceof NodeList) but no IE support
-				json = (typeof selector.length !== 'number');
-				nodes = selector;
-			} else if (typeof selector === 'string') {
-				nodelist = d.querySelectorAll(selector);
-
-				// Convert node list to array so results have full access to array methods
-				// Array.prototype.slice.call not supported in IE < 9 and often slower than loop anyway
-				for (i = 0; i < nodelist.length; i += 1) {
-					nodes[i] = nodelist[i];
-				}
-			}
-		}
-		
-		// Only attach nodes if not JSON
-		cb = json ? {} : nodes;
-		
-		// Public functions
-
-		// Fire on DOM ready
-		cb.ready = function (fn) {
-			if (fn) {
-				if (domready) {
-					fn();
-					return cb;
-				} else {
-					readyfn.push(fn);
-				}
-			}
-		};
-		// Fire on page loaded
-		cb.loaded = function (fn) {
-			if (fn) {
-				if (pageloaded) {
-					fn();
-					return cb;
-				} else {
-					loadedfn.push(fn);
-				}
-			}
-		};
-		
-		// Executes a function on nodes
-		cb.each = function (fn) {
-			if (typeof fn === 'function') {
-				nodeLoop(function (elm) {
-					// <= IE 8 loses scope so need to apply
-					return fn.apply(elm, arguments);
-				}, nodes);
-			}
-			return cb;
-		};
-		
-		return cb;
-	}
-
-	// Set Chibi's global namespace here ($)
-	w.$ = wled;
-
-}());
+import $ from './dom.mjs'
 
 function setLabel(elm) {
-	const id = elm.id;
-	const label = lang.labels[id];
-	elm.textContent = label ? label : id;
-}
-
-//startup, called on page load
-function S() {
-  
+    const id = elm.id;
+    const label = lang.labels[id];
+    elm.textContent = label ? label : id;
 }
 
 //toggle between hidden and 100% width (screen < ? px) 
 //toggle between icons-only and 100% width (screen < ?? px)
 //toggle between icons-only and ? px (screen >= ?? px)
-function menu() {
 
-}
+const menuItems = [
+    { "icon": "&#xe90c;", "id": "e-nw" },
+    { "icon": "&#xe90a;", "id": "e-hw" },
+    { "icon": "&#xe904;", "id": "e-ui" },
+    { "icon": "&#xe906;", "id": "e-if" },
+    { "icon": "&#xe90b;", "id": "e-tm" },
+    { "icon": "&#xe90a;", "id": "e-dx" },
+    { "icon": "&#xe903;", "id": "e-sr" },
+    { "icon": "&#xe907;", "id": "e-um" },
+    { "icon": "&#xe909;", "id": "e-ab" }
+]
 
-//populate labels when to dom is ready but before it is rendered
-$().ready( function() { $('.l').each(setLabel); } );
+// populate labels when to dom is ready but before it is rendered
+$().ready(function() {
+
+    const menu = $('#menu');
+    menuItems.map(item =>
+        menu.htmlAppend(`<div class="entry"><div class="e-icon"><i class="icons">${item.icon}</i></div><div class="l e-label" id="${item.id}"></div></div>`)
+    );
+
+    $('.l').each(setLabel);
+});
---
+++ wled00/data/dom.mjs
@@ -0,0 +1,125 @@
+/*
+ ** DOM module - base on https://github.com/kylebarrow/chibi with IE hacks removed
+ **
+ */
+var readyfn = [],
+    loadedfn = [],
+    domready = false,
+    pageloaded = false,
+    d = document,
+    w = window;
+
+// Fire any function calls on ready event
+function fireReady() {
+    var i;
+    domready = true;
+    for (i = 0; i < readyfn.length; i += 1) {
+        readyfn[i]();
+    }
+    readyfn = [];
+}
+
+// Fire any function calls on loaded event
+function fireLoaded() {
+    var i;
+    pageloaded = true;
+    for (i = 0; i < loadedfn.length; i += 1) {
+        loadedfn[i]();
+    }
+    loadedfn = [];
+}
+
+// Check DOM ready, page loaded
+if (d.addEventListener) {
+    // Standards
+    d.addEventListener('DOMContentLoaded', fireReady, false);
+    w.addEventListener('load', fireLoaded, false);
+}
+
+// Loop through node array
+function nodeLoop(fn, nodes) {
+    var i;
+    // Good idea to walk up the DOM
+    for (i = nodes.length - 1; i >= 0; i -= 1) {
+        fn(nodes[i]);
+    }
+}
+
+function dom(selector) {
+    var self, nodes = [],
+        json = false,
+        nodelist;
+
+    if (selector) {
+
+        // Element node
+        if (selector instanceof HTMLElement) {
+            nodes = [selector]; // return element as node list
+        } else if (selector instanceof NodeList) {
+            // JSON, document object or node list
+            json = (typeof selector.length !== 'number');
+            nodes = selector;
+        } else if (typeof selector === 'string') {
+            nodelist = d.querySelectorAll(selector);
+            nodes = Array.from(nodelist);
+        }
+    }
+
+    // Only attach nodes if not JSON
+    self = json ? {} : nodes;
+
+    // Public functions
+
+    // Fire on DOM ready
+    self.ready = function(fn) {
+        if (fn) {
+            if (domready) {
+                fn();
+                return self;
+            } else {
+                readyfn.push(fn);
+            }
+        }
+    };
+    // Fire on page loaded
+    self.loaded = function(fn) {
+        if (fn) {
+            if (pageloaded) {
+                fn();
+                return self;
+            } else {
+                loadedfn.push(fn);
+            }
+        }
+    };
+
+    // Executes a function on nodes
+    self.each = function(fn) {
+        if (typeof fn === 'function') {
+            nodeLoop(fn, nodes);
+        }
+        return self;
+    };
+
+    self.first = function() {
+        return dom(nodes.shift());
+    };
+
+    // Find last
+    self.last = function() {
+        return dom(nodes.pop());
+    };
+
+    self.htmlAppend = function(value) {
+        if (value) {
+            nodeLoop(function(elm) {
+                elm.insertAdjacentHTML('beforeend', value);
+            }, nodes);
+        }
+        return self;
+    };
+
+    return self;
+}
+
+export default dom;
