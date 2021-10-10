06b26315cd9829f9b10a3511e0acbd76d2cff6cb
Phil Bolduc <philbolduc@gmail.com>
2021-10-09 23:00:26 -0700

    simply self executing function

--- wled00/data/cfg_lang.js
+++ wled00/data/cfg_lang.js
@@ -1,12 +1,10 @@
 (function() {
     // self executing function to ensure translations is set on page load
     // so we dont have to wait for fetch/xhr request
-    var translations = {
+    window.translations = {
         "About": "Über",
         "Save": "Speichern",
         "Schedules": "Zeitpläne",
         "Sound Reactive": "Tonreaktiv"
     };
-
-    window.translations = translations;
 }());
