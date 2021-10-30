41aad107b1331d4050c6a8a77ce40b1080e682b4
Jeff Cooper <jeff@thejeffcooper.com>
2021-10-29 22:48:32 -0400

    commit

--- wled00/wled.h
+++ wled00/wled.h
@@ -32,7 +32,7 @@
   #define WLED_ENABLE_MQTT         // saves 12kb
 #endif
 #define WLED_ENABLE_ADALIGHT       // saves 500b only
-//#define WLED_ENABLE_DMX          // uses 3.5kb (use LEDPIN other than 2)
+#define WLED_ENABLE_DMX          // uses 3.5kb (use LEDPIN other than 2)
 #define WLED_ENABLE_LOXONE         // uses 1.2kb
 #ifndef WLED_DISABLE_WEBSOCKETS
   #define WLED_ENABLE_WEBSOCKETS
