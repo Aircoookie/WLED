f4c05c67a229345d93200eddd768f90e6ccd810f
Blaz Kristan <blaz@kristan-sp.si>
2022-01-22 23:35:00 +0100

    Minor tweak.

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2201221
+#define VERSION 2201222
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
