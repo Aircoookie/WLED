beefe6634ddde7894bf36fa378484f8d97570e88
siggel <siggel-apps@gmx.de>
2022-02-06 23:20:58 +0100

    This reverts commit f8db06c7c5cfe159f9fc03ebc08547fa3c9e9ebd.

--- wled00/presets.cpp
+++ wled00/presets.cpp
@@ -112,7 +112,6 @@ void savePreset(byte index, bool persist, const char* pname, JsonObject saveobj)
   }
   if (persist) presetsModifiedTime = toki.second(); //unix time
   updateFSInfo();
-  alexaInit();
 }
 
 void deletePreset(byte index) {
