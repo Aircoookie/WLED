6eff2e6e74fe53861be40887110fabd265404d34
Blaz Kristan <blaz@kristan-sp.si>
2022-03-28 22:43:51 +0200

    Missing .h

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2203281
+#define VERSION 2203282
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
