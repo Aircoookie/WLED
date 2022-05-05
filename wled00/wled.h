bef9c68f8152cad463fc8dbf6c143d6c81ea3434
Luke Plassman <40614799+lplassman@users.noreply.github.com>
2022-05-04 20:28:09 -0400

    Working DMX Libraries (#2652)
    * added SparkFunDMX library dependencies
    
    * changed variable names to avoid conflicts with SparkFunDMX library
    
    * board version checks
    
    * minor changes to DMX
    
    * fix brightness when no shutter DMX channel is set
    
    * fix compile issue on newer ESP32 variants

--- wled00/wled.h
+++ wled00/wled.h
@@ -111,7 +111,11 @@
 #endif
 
 #ifdef WLED_ENABLE_DMX
+ #ifdef ESP8266
   #include "src/dependencies/dmx/ESPDMX.h"
+ #else //ESP32
+  #include "src/dependencies/dmx/SparkFunDMX.h"
+ #endif  
 #endif
 
 #include "src/dependencies/e131/ESPAsyncE131.h"
@@ -347,7 +351,11 @@ WLED_GLOBAL bool arlsDisableGammaCorrection _INIT(true);          // activate if
 WLED_GLOBAL bool arlsForceMaxBri _INIT(false);                    // enable to force max brightness if source has very dark colors that would be black
 
 #ifdef WLED_ENABLE_DMX
-WLED_GLOBAL DMXESPSerial dmx;
+ #ifdef ESP8266
+  WLED_GLOBAL DMXESPSerial dmx;
+ #else //ESP32
+  WLED_GLOBAL SparkFunDMX dmx; 
+ #endif 
 WLED_GLOBAL uint16_t e131ProxyUniverse _INIT(0);                  // output this E1.31 (sACN) / ArtNet universe via MAX485 (0 = disabled)
 #endif
 WLED_GLOBAL uint16_t e131Universe _INIT(1);                       // settings for E1.31 (sACN) protocol (only DMX_MODE_MULTIPLE_* can span over consequtive universes)
