5e62d9927cd27ba814e9e89ee0ee0da800bb0980
Moa Ziegler <moa.ziegler@infarm.com>
2022-07-26 21:29:33 +0200

    Revert "Revert "Added support for ADXL345""
    This reverts commit 35796a9c3047cdd2bd22cb4d272e73a6f39cead0.

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -128,6 +128,10 @@
 #include "../usermods/Si7021_MQTT_HA/usermod_si7021_mqtt_ha.h"
 #endif
 
+#ifdef USERMOD_ADXL345_SENSOR
+#include "../usermods/usermod_v2_adxl345_sensor/usermod_v2_adxl345_sensor.h"
+#endif
+
 void registerUsermods()
 {
 /*
@@ -242,5 +246,9 @@ void registerUsermods()
   
   #ifdef USERMOD_SI7021_MQTT_HA
   usermods.add(new Si7021_MQTT_HA());
+  #endif
+  
+  #ifdef USERMOD_ADXL345_SENSOR
+  usermods.add(new ADXL345SensorUsermod());
   #endif
 }
