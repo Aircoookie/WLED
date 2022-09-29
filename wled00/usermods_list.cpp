1405a40ed31fb18f4311b940934cfa1cace38320
Steven Dahsevsky <dashevskysteven@gmail.com>
2022-09-29 12:17:44 +0300

    Implemented usermod for integration with smartnest.cz

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -128,6 +128,10 @@
 #include "../usermods/Si7021_MQTT_HA/usermod_si7021_mqtt_ha.h"
 #endif
 
+#ifdef USERMOD_SMARTNEST
+#include "../usermods/smartnest/usermod_smartnest.h"
+#endif
+
 void registerUsermods()
 {
 /*
@@ -242,5 +246,9 @@ void registerUsermods()
   
   #ifdef USERMOD_SI7021_MQTT_HA
   usermods.add(new Si7021_MQTT_HA());
+  #endif
+  
+  #ifdef USERMOD_SMARTNEST
+  usermods.add(new Smartnest());
   #endif
 }
