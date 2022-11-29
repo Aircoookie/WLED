125017f57d9266c5840e7a15dc650db2fdff7d14
Frank <frank.moehle@outlook.de>
2022-11-29 22:20:37 +0100

    small updates
    - colors: add clarification that alternative table does not affect NeopPixelBus colors
    - npm run build
    - build number up

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2211290
+#define VERSION 2211291
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
