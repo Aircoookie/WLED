00fed4f995c8936433b77939ec67bb7550efa9d7
Blaz Kristan <blaz@kristan-sp.si>
2022-12-18 11:33:13 +0100

    Carifications and implicit HW_PIN... definition

--- wled00/wled.h
+++ wled00/wled.h
@@ -676,32 +676,32 @@ WLED_GLOBAL uint16_t ledMaps _INIT(0); // bitfield representation of available l
 // Usermod manager
 WLED_GLOBAL UsermodManager usermods _INIT(UsermodManager());
 
-// global I2C SDA pin [HW_PIN_SDA] (used for usermods)
+// global I2C SDA pin (used for usermods)
 #ifndef I2CSDAPIN
 WLED_GLOBAL int8_t i2c_sda  _INIT(-1);
 #else
 WLED_GLOBAL int8_t i2c_sda  _INIT(I2CSDAPIN);
 #endif
-// global I2C SCL pin [HW_PIN_SCL] (used for usermods)
+// global I2C SCL pin (used for usermods)
 #ifndef I2CSCLPIN
 WLED_GLOBAL int8_t i2c_scl  _INIT(-1);
 #else
 WLED_GLOBAL int8_t i2c_scl  _INIT(I2CSCLPIN);
 #endif
 
-// global SPI DATA/MOSI pin [HW_PIN_DATASPI] (used for usermods)
+// global SPI DATA/MOSI pin (used for usermods)
 #ifndef SPIMOSIPIN
 WLED_GLOBAL int8_t spi_mosi  _INIT(-1);
 #else
 WLED_GLOBAL int8_t spi_mosi  _INIT(SPIMOSIPIN);
 #endif
-// global SPI DATA/MISO pin [HW_PIN_MISOSPI] (used for usermods)
+// global SPI DATA/MISO pin (used for usermods)
 #ifndef SPIMISOPIN
 WLED_GLOBAL int8_t spi_miso  _INIT(-1);
 #else
 WLED_GLOBAL int8_t spi_miso  _INIT(SPIMISOPIN);
 #endif
-// global SPI CLOCK/SCLK pin [HW_PIN_CLOCKSPI] (used for usermods)
+// global SPI CLOCK/SCLK pin (used for usermods)
 #ifndef SPISCLKPIN
 WLED_GLOBAL int8_t spi_sclk  _INIT(-1);
 #else
