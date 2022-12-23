0e236f9d88a81fb23b143153cae5dc123256b309
cschwinne <dev.aircoookie@gmail.com>
2022-12-23 04:38:30 +0100

    0.14.0-b1

--- wled00/wled.h
+++ wled00/wled.h
@@ -3,12 +3,12 @@
 /*
    Main sketch, global variable declarations
    @title WLED project sketch
-   @version 0.14.0-b0
+   @version 0.14.0-b1
    @author Christian Schwinne
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2212220
+#define VERSION 2212222
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
