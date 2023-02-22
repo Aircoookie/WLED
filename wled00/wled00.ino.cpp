62b1422111228a4337505f0f67e0c96887fa8db2
Frank <91616163+softhack007@users.noreply.github.com>
2023-02-22 18:06:34 +0100

    enable core debug messages in 16MB_max_debug configs

---
+++ wled00/wled00.ino.cpp
@@ -0,0 +1,15 @@
+# 1 "C:\\Users\\Frank\\AppData\\Local\\Temp\\tmpewo3dp0a"
+#include <Arduino.h>
+# 1 "D:/ARDUINO_Work/WORK/#ESP32/WLED_soundreactive-GitHub_DEV/WLED_MoonModules/WLED_mdev_clean/wled00/wled00.ino"
+# 13 "D:/ARDUINO_Work/WORK/#ESP32/WLED_soundreactive-GitHub_DEV/WLED_MoonModules/WLED_mdev_clean/wled00/wled00.ino"
+#include "wled.h"
+void setup();
+void loop();
+#line 15 "D:/ARDUINO_Work/WORK/#ESP32/WLED_soundreactive-GitHub_DEV/WLED_MoonModules/WLED_mdev_clean/wled00/wled00.ino"
+void setup() {
+  WLED::instance().setup();
+}
+
+void loop() {
+  WLED::instance().loop();
+}
