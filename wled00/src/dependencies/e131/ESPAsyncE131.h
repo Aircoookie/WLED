29e6c98e59296cfe6539f24a9e44042efa66f601
coral <jonas@jonasbengtson.se>
2023-05-02 10:07:07 -0700

    fix DDP spec

--- wled00/src/dependencies/e131/ESPAsyncE131.h
+++ wled00/src/dependencies/e131/ESPAsyncE131.h
@@ -53,8 +53,8 @@ typedef struct ip_addr ip4_addr_t;
 #define DDP_PUSH_FLAG 0x01
 #define DDP_TIMECODE_FLAG 0x10
 
-#define DDP_TYPE_RGB24  0x0A
-#define DDP_TYPE_RGBW32 0x1A
+#define DDP_TYPE_RGB24  0x0D
+#define DDP_TYPE_RGBW32 0x1E
 
 #define ARTNET_OPCODE_OPDMX 0x5000
 #define ARTNET_OPCODE_OPPOLL 0x2000
