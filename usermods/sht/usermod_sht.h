cb740d34d7339790a1a14fbe62ea07295fec6c7d
Blaz Kristan <blaz@kristan-sp.si>
2023-05-10 21:09:28 +0200

    Bugfix - SHT enable/disable crash

--- usermods/sht/usermod_sht.h
+++ usermods/sht/usermod_sht.h
@@ -22,7 +22,7 @@ class ShtUsermod : public Usermod
     bool haMqttDiscoveryDone = false; // Remembers if we already published the HA discovery topics
 
     // SHT vars
-    SHT *shtTempHumidSensor; // Instance of SHT lib
+    SHT *shtTempHumidSensor = nullptr; // Instance of SHT lib
     byte shtType = 0; // SHT sensor type to be used. Default: SHT30
     byte unitOfTemp = 0; // Temperature unit to be used. Default: Celsius (0 = Celsius, 1 = Fahrenheit)
     bool shtInitDone = false; // Remembers if SHT sensor has been initialised
@@ -37,7 +37,7 @@ class ShtUsermod : public Usermod
     void initShtTempHumiditySensor();
     void cleanupShtTempHumiditySensor();
     void cleanup();
-    bool isShtReady();
+    inline bool isShtReady() { return shtInitDone; } // Checks if the SHT sensor has been initialised.
 
     void publishTemperatureAndHumidityViaMqtt();
     void publishHomeAssistantAutodiscovery();
@@ -117,8 +117,11 @@ void ShtUsermod::initShtTempHumiditySensor()
  */
 void ShtUsermod::cleanupShtTempHumiditySensor()
 {
-  if (isShtReady()) shtTempHumidSensor->reset();
-  delete shtTempHumidSensor;
+  if (isShtReady()) {
+    shtTempHumidSensor->reset();
+    delete shtTempHumidSensor;
+    shtTempHumidSensor = nullptr;
+  }
   shtInitDone = false;
 }
 
@@ -144,16 +147,6 @@ void ShtUsermod::cleanup()
 
   enabled = false;
   shtInitDone = false; // WLEDMM bugfix
-}
-
-/**
- * Checks if the SHT sensor has been initialised.
-  *
- * @return bool
- */
-bool ShtUsermod::isShtReady()
-{
-  return shtInitDone;
 }
 
 /**
@@ -496,7 +489,19 @@ void ShtUsermod::addToJsonInfo(JsonObject& root)
   jsonHumidity.add(F(" RH"));
 
   jsonTemp.add(getTemperature());
-  jsonTemp.add(unitOfTemp ? "°F" : "°C");
+  jsonTemp.add(getUnitString());
+
+  // sensor object
+  JsonObject sensor = root[F("sensor")];
+  if (sensor.isNull()) sensor = root.createNestedObject(F("sensor"));
+
+  jsonTemp = sensor.createNestedArray(F("temp"));
+  jsonTemp.add(getTemperature());
+  jsonTemp.add(getUnitString());
+
+  jsonHumidity = sensor.createNestedArray(F("humidity"));
+  jsonHumidity.add(getHumidity());
+  jsonHumidity.add(F(" RH"));
 }
 
 /**
