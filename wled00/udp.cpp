1c8f349a625d4c8285961b0df3096db0ab421484
Blaz Kristan <blaz@kristan-sp.si>
2023-05-14 20:30:57 +0200

    Bugfix. - prevent LED flash on realtime end

--- wled00/udp.cpp
+++ wled00/udp.cpp
@@ -189,7 +189,7 @@ void realtimeLock(uint32_t timeoutMs, byte md)
 void exitRealtime() {
   if (!realtimeMode) return;
   if (realtimeOverride == REALTIME_OVERRIDE_ONCE) realtimeOverride = REALTIME_OVERRIDE_NONE;
-  strip.setBrightness(scaledBri(bri));
+  strip.setBrightness(scaledBri(bri), true);
   realtimeTimeout = 0; // cancel realtime mode immediately
   realtimeMode = REALTIME_MODE_INACTIVE; // inform UI immediately
   realtimeIP[0] = 0;
