d383bc93c744064ba2a17a661ec5c16403b5484e
Aircoookie <21045690+Aircoookie@users.noreply.github.com>
2023-06-02 10:51:37 +0200

    Changelog update
    Reduce width of ethernet mode dropdown

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2305280
+#define VERSION 2306020
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
