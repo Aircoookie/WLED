423d3720b2892cfefc9a0c85cf702fc00c30d8b9
Frank <91616163+softhack007@users.noreply.github.com>
2023-07-13 21:44:12 +0200

    post-merge
    * disabled FASTPATH breaking changes in normal builds
    * _S builds for esp32 stay fully FASTPATH enabled
    * full movie quote
    * npm run build

--- wled00/data/index.js
+++ wled00/data/index.js
@@ -676,7 +676,7 @@ function populateInfo(i)
 	if (i.ver.includes("0.14.1")) vcn = "Sitting Ducks"; // easter egg
 	if (i.ver.includes("0.14.0")) vcn = "Lupo";          // check for MM versioning scheme
 	if (i.ver.includes("0.14.0-b15.1")) vcn = "Sitting Ducks"; // late easter egg
-	if (i.ver.includes("0.14.0-b15.2")) vcn = "The Way"; // recently watched The Mandalorian?
+	if (i.ver.includes("0.14.0-b15.2")) vcn = "This is the way"; // recently watched The Mandalorian? I have spoken ;-)
 	if (i.ver.includes("0.14.0-b15.22")) vcn = "Lupo";
 	cn += `v${i.ver} &nbsp;<i>"${vcn}"</i><p>(WLEDMM_${i.ver} ${i.rel}.bin)</p><p><em>build ${i.vid}</em></p><table>
 ${urows}
