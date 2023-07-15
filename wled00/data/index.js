ead76be9a38a1233d4bd0dc3b0b25d3c8c7f4093
Frank <91616163+softhack007@users.noreply.github.com>
2023-07-15 13:19:08 +0200

    Update index.js
    Small update for upcoming release

--- wled00/data/index.js
+++ wled00/data/index.js
@@ -675,8 +675,8 @@ function populateInfo(i)
 	//WLEDMM: add total heap and total PSRAM, and build number, add bin name
 	if (i.ver.includes("0.14.1")) vcn = "Sitting Ducks"; // easter egg
 	if (i.ver.includes("0.14.0")) vcn = "Lupo";          // check for MM versioning scheme
-	if (i.ver.includes("0.14.0-b15.1")) vcn = "Sitting Ducks"; // late easter egg
-	if (i.ver.includes("0.14.0-b15.2")) vcn = "This is the way"; // recently watched The Mandalorian? I have spoken ;-)
+	if (i.ver.includes("0.14.0-b15")) vcn = "Sitting Ducks"; // late easter egg
+	if (i.ver.includes("0.14.0-b25")) vcn = "This is the way"; // recently watched The Mandalorian? I have spoken ;-)
 	if (i.ver.includes("0.14.0-b15.22")) vcn = "Lupo";
 	cn += `v${i.ver} &nbsp;<i>"${vcn}"</i><p>(WLEDMM_${i.ver} ${i.rel}.bin)</p><p><em>build ${i.vid}</em></p><table>
 ${urows}
