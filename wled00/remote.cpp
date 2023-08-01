c13b32e06a37c96fd971dd1303338f95ebca6d25
Frank <91616163+softhack007@users.noreply.github.com>
2023-08-01 11:53:32 +0200

    esp-now remote: fix crash with AP = Always
    initialize ESPNOW only when Wifi (STA or AP) is running

--- wled00/remote.cpp
+++ wled00/remote.cpp
@@ -43,7 +43,7 @@ typedef struct message_structure {
 } message_structure;
 
 static int esp_now_state = ESP_NOW_STATE_UNINIT;
-static uint32_t last_seq = -1;
+static uint32_t last_seq = UINT32_MAX;
 static int brightnessBeforeNightMode = NIGHT_MODE_DEACTIVATED;
 static message_structure incoming;
 
@@ -172,7 +172,7 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
 
 void handleRemote() {
   if (enable_espnow_remote) {
-    if (esp_now_state == ESP_NOW_STATE_UNINIT) {
+    if ((esp_now_state == ESP_NOW_STATE_UNINIT) && (interfacesInited || apActive)) { // ESPNOW requires Wifi to be initialized (either STA, or AP Mode) 
       USER_PRINTLN(F("\nInitializing ESP_NOW listener!\n"));
       // Init ESP-NOW
       if (esp_now_init() != ESP_OK) {
