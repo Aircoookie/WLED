e94064addeb04a19745a71378010fa514ccdf7c9
Blaz Kristan <blaz@kristan-sp.si>
2023-08-28 17:58:30 +0200

    Bugfix. - feed WDT even if strip is updating - provide custom palette names - handle interface cooldown properly - rotary encoder ALT fix for custom palettes

--- wled00/led.cpp
+++ wled00/led.cpp
@@ -161,6 +161,8 @@ void stateUpdated(byte callMode) {
 
 void updateInterfaces(uint8_t callMode)
 {
+  if (!interfaceUpdateCallMode || millis() - lastInterfaceUpdate < INTERFACE_UPDATE_COOLDOWN) return;
+
   sendDataWs();
   lastInterfaceUpdate = millis();
   interfaceUpdateCallMode = 0; //disable
@@ -180,7 +182,7 @@ void updateInterfaces(uint8_t callMode)
 void handleTransitions()
 {
   //handle still pending interface update
-  if (interfaceUpdateCallMode && millis() - lastInterfaceUpdate > INTERFACE_UPDATE_COOLDOWN) updateInterfaces(interfaceUpdateCallMode);
+  updateInterfaces(interfaceUpdateCallMode);
 #ifndef WLED_DISABLE_MQTT
   if (doPublishMqtt) publishMqtt();
 #endif
