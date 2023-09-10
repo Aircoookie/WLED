0d3a1b6fe0216d9fe203dac118bc57e574247c3d
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-09-10 23:39:33 +1000

    revert

--- wled00/FX.h
+++ wled00/FX.h
@@ -321,7 +321,7 @@
 #define FX_MODE_ROCKTAVES              185
 #define FX_MODE_2DAKEMI                186
 
-#define MODE_COUNT                     188
+#define MODE_COUNT                     187
 
 typedef enum mapping1D2D {
   M12_Pixels = 0,
