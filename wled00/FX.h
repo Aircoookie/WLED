193195971856e1578e1b968bc0832a1a2b956958
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-09-10 23:40:26 +1000

    whitespace

--- wled00/FX.h
+++ wled00/FX.h
@@ -181,7 +181,7 @@
 #define FX_MODE_TWO_DOTS                50
 #define FX_MODE_FAIRYTWINKLE            51  //was Two Areas prior to 0.13.0-b6 (use "Two Dots" with full intensity)
 #define FX_MODE_RUNNING_DUAL            52
-#define FX_MODE_BOX_BREATH                  53
+#define FX_MODE_BOX_BREATH              53
 // #define FX_MODE_HALLOWEEN               53  // removed in 0.14!
 #define FX_MODE_TRICOLOR_CHASE          54
 #define FX_MODE_TRICOLOR_WIPE           55
