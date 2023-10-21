501c6749bc493fcded9eb4a8548a4e70623225f3
Will Tatam <will@netmindz.net>
2023-10-21 20:00:07 +0100

    Use share gamma table

--- usermods/usermod_v2_animartrix/usermod_v2_animartrix.h
+++ usermods/usermod_v2_animartrix/usermod_v2_animartrix.h
@@ -87,24 +87,6 @@ static const char _data_FX_mode_Waves[] PROGMEM = "Y💡Waves ☾@Speed;;1;2";
 static const char _data_FX_mode_Chasing_Spirals[] PROGMEM = "Y💡Chasing_Spirals ☾@Speed;;1;2";
 static const char _data_FX_mode_Rotating_Blob[] PROGMEM = "Y💡Rotating_Blob ☾@Speed;;1;2";
 
-const uint8_t PROGMEM gamma8[] = {
-	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-	1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-	2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-	5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-	10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-	17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-	25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-	37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-	51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-	69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-	90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-	115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-	144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-	177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-	215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
-
 class ANIMartRIXMod:public ANIMartRIX {
 	public:
 	void initEffect() {
@@ -122,21 +104,22 @@ class ANIMartRIXMod:public ANIMartRIX {
 	}
 	void setPixelColor(int x, int y, rgb pixel) {
 		SEGMENT.setPixelColorXY(x, y, CRGB(
-			pgm_read_byte(&gamma8[pixel.red]),
-			pgm_read_byte(&gamma8[pixel.green]),
-			pgm_read_byte(&gamma8[pixel.blue])
+			pgm_read_byte(&gamma8[(uint8_t) pixel.red]),
+			pgm_read_byte(&gamma8[(uint8_t) pixel.green]),
+			pgm_read_byte(&gamma8[(uint8_t) pixel.blue])
 		));
 	}
 	void setPixelColor(int index, rgb pixel) {
 		SEGMENT.setPixelColor(index, CRGB(
-			pgm_read_byte(&gamma8[pixel.red]),
-			pgm_read_byte(&gamma8[pixel.green]),
-			pgm_read_byte(&gamma8[pixel.blue])
+			pgm_read_byte(&gamma8[(uint8_t) pixel.red]),
+			pgm_read_byte(&gamma8[(uint8_t) pixel.green]),
+			pgm_read_byte(&gamma8[(uint8_t) pixel.blue])
 		));
   	}
 
 	// Add any extra custom effects not part of the ANIMartRIX libary here
 
+	private:
 };
 ANIMartRIXMod anim;
 
