ca0eb5313ba1b71b9379c0c5edaf03723e874395
Will Tatam <will@netmindz.net>
2023-10-24 22:45:06 +0100

    Swap to 64x64

--- wled00/bus_manager.cpp
+++ wled00/bus_manager.cpp
@@ -464,9 +464,8 @@ void BusNetwork::cleanup() {
 
 BusSmartMatrix::BusSmartMatrix(BusConfig &bc) : Bus(bc.type, bc.start, bc.autoWhite) {
 
-  #define num_x       32                       // how many LEDs are in one row?
-  #define num_y       32                       // how many rows?
-  #define brightness 255                       // please be aware that reducing brightness also reduces color resolution, use only in emergency
+  #define num_x       64                       // how many LEDs are in one row?
+  #define num_y       64                       // how many rows?
 
   #define radial_filter_radius 23.0;      // on 32x32, use 11 for 16x16
 
@@ -475,7 +474,7 @@ BusSmartMatrix::BusSmartMatrix(BusConfig &bc) : Bus(bc.type, bc.start, bc.autoWh
   const uint16_t kMatrixHeight  = num_y;  // Set to the height of your display
   const uint8_t  kRefreshDepth  = 36;     // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
   const uint8_t  kDmaBufferRows = 4;      // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
-  const uint8_t  kPanelType              = SM_PANELTYPE_HUB75_32ROW_MOD16SCAN;   // Choose the configuration that matches your panels.  See more details in MatrixCommonHub75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
+  const uint8_t  kPanelType              = SM_PANELTYPE_HUB75_64ROW_MOD32SCAN;   // Choose the configuration that matches your panels.  See more details in MatrixCommonHub75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
   const uint32_t kMatrixOptions          = (SM_HUB75_OPTIONS_NONE);        // see docs for options: https://github.com/pixelmatix/SmartMatrix/wiki
   const uint8_t  kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
 
