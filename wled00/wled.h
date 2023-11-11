adc723c72c16e66f6ca5752e2ac0066afe6912e6
Frank <frank.moehle@outlook.de>
2023-11-11 23:58:02 +0100

    increment version, npm run build
    added a few fixes and minor improvements from upstream (0_14_1 and 0_15_0 branch)

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2310240
+#define VERSION 2311110
 
 //WLEDMM + Moustachauve/Wled-Native 
 // You can define custom product info from build flags.
