e7c34e9e47e63f5ed57679ff40b839920dcef749
Frank <91616163+softhack007@users.noreply.github.com>
2023-11-27 18:19:31 +0100

    chores
    * npm run build
    * build number ++

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2311271
+#define VERSION 2311272
 
 //WLEDMM + Moustachauve/Wled-Native 
 // You can define custom product info from build flags.
