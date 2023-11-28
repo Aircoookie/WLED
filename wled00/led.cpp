ea76a98a8361ada45d85a9a2cd92486c25053e2d
Blaz Kristan <blaz@kristan-sp.si>
2023-11-28 10:42:18 +0100

    Possible fix for #3541

--- wled00/led.cpp
+++ wled00/led.cpp
@@ -137,8 +137,6 @@ void stateUpdated(byte callMode) {
     if (strip.getTransition() == 0) {
       jsonTransitionOnce = false;
       transitionActive = false;
-      applyFinalBri();
-      strip.trigger();
       return;
     }
 
@@ -162,7 +160,7 @@ void updateInterfaces(uint8_t callMode)
 
   sendDataWs();
   lastInterfaceUpdate = millis();
-  interfaceUpdateCallMode = 0; //disable further updates
+  interfaceUpdateCallMode = 0; //disable
 
   if (callMode == CALL_MODE_WS_SEND) return;
 
@@ -225,7 +223,7 @@ void handleNightlight()
     if (!nightlightActiveOld) //init
     {
       nightlightStartTime = millis();
-      nightlightDelayMs = (unsigned)(nightlightDelayMins*60000);
+      nightlightDelayMs = (int)(nightlightDelayMins*60000);
       nightlightActiveOld = true;
       briNlT = bri;
       for (byte i=0; i<4; i++) colNlT[i] = col[i]; // remember starting color
