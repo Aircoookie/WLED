2a42fea32a6c0fdc273403ee8b3f7f9c24747392
Janos Stefan <it.stefan.janos@gmail.com>
2023-12-11 09:26:20 +0100

    Add APA106 support

--- wled00/const.h
+++ wled00/const.h
@@ -234,6 +234,7 @@
 #define TYPE_WS2811_400KHZ       24            //half-speed WS2812 protocol, used by very old WS2811 units
 #define TYPE_TM1829              25
 #define TYPE_UCS8903             26
+#define TYPE_APA106              27
 #define TYPE_UCS8904             29
 #define TYPE_SK6812_RGBW         30
 #define TYPE_TM1814              31
