7624b7618662fe1e8255e2f340869c310c979389
Frank <frank.moehle@outlook.de>
2023-12-16 22:58:14 +0100

    version bump 0.14.0-b28.34

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2312150
+#define VERSION 2312160
 
 //WLEDMM + Moustachauve/Wled-Native 
 // You can define custom product info from build flags.
