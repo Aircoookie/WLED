21a937232053603821c3ae4ffdbde6235ee47928
Woody <xaver@holzapfel.net>
2024-01-25 12:14:44 +0100

    fix clock overlay display

--- wled00/overlay.cpp
+++ wled00/overlay.cpp
@@ -91,8 +91,9 @@ void handleOverlayDraw() {
   usermods.handleOverlayDraw();
   if (analogClockSolidBlack) {
     const Segment* segments = strip.getSegments();
-    for (uint8_t i = 0; i < strip.getActiveSegmentsNum(); i++) {
+    for (uint8_t i = 0; i < strip.getSegmentsNum(); i++) {
       const Segment& segment = segments[i];
+      if (!segment.isActive()) continue;
       if (segment.mode > 0 || segment.colors[0] > 0) {
         return;
       }
