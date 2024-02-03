da60788f7df2f77fa82cda29e4abd54d2036dd5a
ale1800 <53172176+ale1800@users.noreply.github.com>
2024-02-03 14:09:57 +0100

    Add files via upload

--- usermods/Bar_Timer/usermod_timer_bar.h
+++ usermods/Bar_Timer/usermod_timer_bar.h
@@ -6,8 +6,8 @@
 #define STOPPED 0
 
 /**
- * This class create a timer using the connected strip. When the timer starts, the strip is filled with the color of the solid effect.
- * Led are turned off according to the remaining time of the timer.
+ * This class create a timer using the connected strip. When the timer starts, the strip is filled with green color.
+ * Led are turned off according to the remaining time of the timer and the color starts changing going to red when the timer has ended.
  * MQTT messages to use the timer are:
  * 
  * 1) (device topic)/timerbar with payload "+X" -> X is the duration of the timer in seconds. If there's no active timer, with this command 
@@ -31,27 +31,24 @@ class UsermodTimerBar : public Usermod {
 
     private:
 
-        uint8_t timer_status;
-        uint16_t number_of_leds, led_turn_off = 0;
-        uint32_t millis_wait_time; 
-        uint16_t start_time, end_time;
-        Segment main;
-        byte settings[3];
-        int helper  = 0;
+        struct Timer{
+            
+          uint8_t id;
+          uint32_t wait_time;
+          uint8_t status;
+        };
 
+        Timer timer;
+
+        uint16_t number_of_leds, led_turn_off = 0;
+        uint32_t start_time, end_time;
 
         void stop_timer() {
 
-          led_turn_off = 0;
-          
-          //Restore previous strip settings
-          effectSpeed = settings[1];
-          effectPalette = settings[2];
-          strip.setMode(0, effectCurrent);
-
           publishMqtt("END", false);
 
-          timer_status = STOPPED;
+          led_turn_off = 0;
+          timer.status = STOPPED;
         }
 
 
@@ -59,7 +56,7 @@ class UsermodTimerBar : public Usermod {
             
           end_time = millis();
 
-          if ((end_time - start_time) >= millis_wait_time) {
+          if ((end_time - start_time) >= timer.wait_time) {
 
             led_turn_off++;
             start_time = end_time;
@@ -77,48 +74,38 @@ class UsermodTimerBar : public Usermod {
 
         void setup() {
 
-          timer_status = STOPPED;
+          timer.status = STOPPED;
           led_turn_off = 0;
         }
     
+
         void loop (){
 
-          if (timer_status == RUNNING) {
-
-            helper = 1;
-            strip.trigger();
-            strip.show();
-
-            for (int y = number_of_leds - 1; y >= number_of_leds - led_turn_off; y--) {
-
-              strip.setPixelColor(y, 0, 0, 0, 0);              
-            }
+          if (timer.status == RUNNING) {
 
             check_timer();
           }
-          helper = 0;
         }
 
 
-        /**
-         * This function is executed before every strip.show(). This timer visualization uses the solid effect. When leds are turned off, the main
-         * WLED software try to restore the color picked from the web UI, so time by time all the turned off leds light up again and then turned off
-         * in the loop function of this usermod. So using this function we can set again the led off before the show() function
-         * 
-         */
         void handleOverlayDraw()
         {
-          if (helper == 0 && timer_status == RUNNING) {
+
+          if (timer.status == RUNNING) {
+
+            float x =  (float) led_turn_off / number_of_leds;
+
+            for (int i = 0; i < number_of_leds - led_turn_off; i++) {
+
+              strip.setPixelColor(i, 0 + round(x*255), 255 - round(x*255), 0, 0);
+            } 
           
-            for (int y = number_of_leds - 1; y >= number_of_leds - 1 - led_turn_off; y--) {
+            for (int y = number_of_leds - 1; y >= number_of_leds - led_turn_off; y--) {
             
-             strip.setPixelColor(y, 0, 0, 0, 0);              
+              strip.setPixelColor(y, 0, 0, 0, 0);              
             }
           }
         }
-
-
-
 
     /**
      * handling of MQTT message
@@ -130,7 +117,7 @@ class UsermodTimerBar : public Usermod {
 
           if (strlen(topic) == 9 && strncmp_P(topic, PSTR("/timerbar"), 9) == 0) {
 
-            if (strcmp("STOP", payload) == 0){
+            if (strncmp_P(payload, PSTR("STOP"), 4) == 0){
 
               stop_timer();
             }
@@ -139,30 +126,17 @@ class UsermodTimerBar : public Usermod {
 
               uint16_t add_time = atoi(payload + 1);
 
-              switch(timer_status) {
+              switch(timer.status) {
 
                 case STOPPED:
 
                   number_of_leds = strip.getLengthTotal();
 
-                  millis_wait_time = (add_time / (float) (number_of_leds-1)) * 1000;
-                  Serial.print(millis_wait_time);
+                  timer.wait_time = (add_time / (float) (number_of_leds)) * 1000;
 
-                  
-
-                  main = strip.getSegment(0);
-
-                  //Save current strip configuration
-                  settings[0] = effectCurrent;
-                  settings[1] = effectSpeed;
-                  settings[2] = effectPalette;
-
-                  strip.setMode(0, FX_MODE_STATIC);
-                  main.fill(SEGCOLOR(0));
+                  timer.status = RUNNING;
 
                   publishMqtt("START", false);
-
-                  timer_status = RUNNING;
 
                   start_time = millis();
 
@@ -172,7 +146,7 @@ class UsermodTimerBar : public Usermod {
 
                   //If the timer is running, add the received time to the timer
 
-                  millis_wait_time = millis_wait_time + (add_time / (float) (number_of_leds - 1 - led_turn_off)) * 1000;
+                  timer.wait_time = timer.wait_time + (add_time / (float) (number_of_leds - led_turn_off)) * 1000;
 
                   break;
               }
@@ -190,7 +164,6 @@ class UsermodTimerBar : public Usermod {
       strlcpy(subuf, mqttDeviceTopic, 33);
       strcat_P(subuf, PSTR("/timerbar"));
       mqtt->subscribe(subuf, 0);
-
     }
 
     void publishMqtt(const char* state, bool retain)
