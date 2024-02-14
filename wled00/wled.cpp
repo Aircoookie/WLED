70be72342f6ca444784f97f3c450d400443bc747
Will Miles <will@willmiles.net>
2024-02-13 19:04:35 -0500

    Debug: Dump web server state

--- wled00/wled.cpp
+++ wled00/wled.cpp
@@ -265,6 +265,7 @@ void WLED::loop()
       DEBUG_PRINT(F("Strip time[ms]: "));  DEBUG_PRINT(avgStripMillis/loops); DEBUG_PRINT("/"); DEBUG_PRINTLN(maxStripMillis);
     }
     strip.printSize();
+    server.dumpStatus();
     loops = 0;
     maxLoopMillis = 0;
     maxUsermodMillis = 0;
