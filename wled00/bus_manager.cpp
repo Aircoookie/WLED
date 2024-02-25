e3e9ff72faa910fb2ebb5d1512f04286fe5e807d
Will Tatam <will@netmindz.net>
2024-02-25 11:24:12 +0000

    Force chain length to 1 for 64 panels

--- wled00/bus_manager.cpp
+++ wled00/bus_manager.cpp
@@ -466,7 +466,6 @@ BusHub75Matrix::BusHub75Matrix(BusConfig &bc) : Bus(bc.type, bc.start, bc.autoWh
 
   mxconfig.double_buff = false; // <------------- Turn on double buffer
 
-  mxconfig.chain_length = max((u_int8_t) 1, min(bc.pins[0], (u_int8_t) 4)); // prevent bad data preventing boot due to low memory
 
   fourScanPanel = nullptr;
 
@@ -491,6 +490,13 @@ BusHub75Matrix::BusHub75Matrix(BusConfig &bc) : Bus(bc.type, bc.start, bc.autoWh
       mxconfig.mx_width = 64 * 2;
       mxconfig.mx_height = 64 / 2;
       break;
+  }
+
+  mxconfig.chain_length = max((u_int8_t) 1, min(bc.pins[0], (u_int8_t) 4)); // prevent bad data preventing boot due to low memory
+
+  if(mxconfig.mx_width >= 64 && (bc.pins[0] > 1)) {
+    USER_PRINT("WARNING, only single panel can be used of 64x64 due to memory")
+    mxconfig.chain_length = 1;
   }
 
   // mxconfig.driver   = HUB75_I2S_CFG::SHIFTREG;
