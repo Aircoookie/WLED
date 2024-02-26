755f91f5abdf28135afc7d5b801d046ac2a9a6ac
Will Tatam <will@netmindz.net>
2024-02-26 21:16:33 +0000

    Remove referece to MM

--- wled00/xml.cpp
+++ wled00/xml.cpp
@@ -771,7 +771,7 @@ void getSettingsJS(byte subPage, char* dest)
     oappend(SET_F("gId(\"somp\").remove(1);")); // remove 2D option from dropdown
     #endif
     #ifndef WLED_ENABLE_HUB75MATRIX
-    oappend(SET_F("hideHub75();"));  // WLEDMM hide HUB75 LED types
+    oappend(SET_F("hideHub75();"));  // hide HUB75 LED types
     #endif    
 
   }
