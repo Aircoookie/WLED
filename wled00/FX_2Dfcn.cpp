6e23b09a93beac8e641e1ff612528c6c85ad40a7
Troy <5659019+troyhacks@users.noreply.github.com>
2024-04-04 21:46:35 -0400

    Update FX_2Dfcn.cpp
    removed temp debugging

--- wled00/FX_2Dfcn.cpp
+++ wled00/FX_2Dfcn.cpp
@@ -64,13 +64,11 @@ void WS2812FX::setUpMatrix() {
       return;
     }
 
-    #ifndef USERMOD_AUTO_PLAYLIST // TroyHacks - FIXME: just tidy output temporarily while debugging
     USER_PRINTF("setUpMatrix %d x %d\n", Segment::maxWidth, Segment::maxHeight);
-    #endif
-
+    
     //WLEDMM recreate customMappingTable if more space needed
     if (Segment::maxWidth * Segment::maxHeight > customMappingTableSize) {
-      size_t size = max(ledmapMaxSize, size_t(Segment::maxWidth * Segment::maxHeight));//TroyHack
+      size_t size = max(ledmapMaxSize, size_t(Segment::maxWidth * Segment::maxHeight)); // TroyHacks
       USER_PRINTF("setupmatrix customMappingTable alloc %d from %d\n", size, customMappingTableSize);
       //if (customMappingTable != nullptr) delete[] customMappingTable;
       //customMappingTable = new uint16_t[size];
