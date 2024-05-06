95c73d80a9b4c4459b7f200382bde5eb072558d5
Adam Matthews <adam@wonkatron.com>
2024-05-06 14:36:37 +0100

    Update Usermod: Battery
    Issue:
    When taking the initial voltage reading while first powering on, voltage hasn't had chance to stabilize so the reading can be inaccurate, which in turn may incorrectly trigger the low-power preset. (Manifests when the user has selected a low read interval and/or is using a capacitor).
    
    Resolution:
    A non-blocking, fixed 10 second delay has been added to the initial voltage reading to give the voltage time to stabilize.

--- usermods/Battery/usermod_v2_Battery.h
+++ usermods/Battery/usermod_v2_Battery.h
@@ -26,6 +26,22 @@ class UsermodBattery : public Usermod
     unsigned long readingInterval = USERMOD_BATTERY_MEASUREMENT_INTERVAL;
     unsigned long nextReadTime = 0;
     unsigned long lastReadTime = 0;
+    // Initial delay before first reading (in milliseconds)
+    unsigned long initialDelay = 10000;
+    // Flag to check if the initial delay is completed
+    bool initialDelayComplete = false;
+    // Flag to check if the initial reading is complete
+    bool isFirstReading = true;
+    // battery min. voltage
+    float minBatteryVoltage = USERMOD_BATTERY_MIN_VOLTAGE;
+    // battery max. voltage
+    float maxBatteryVoltage = USERMOD_BATTERY_MAX_VOLTAGE;
+    // all battery cells summed up
+    unsigned int totalBatteryCapacity = USERMOD_BATTERY_TOTAL_CAPACITY;
+    // raw analog reading 
+    float rawValue = 0.0f;
+    // calculated voltage            
+    float voltage = maxBatteryVoltage;
     // between 0 and 1, to control strength of voltage smoothing filter
     float alpha = USERMOD_BATTERY_AVERAGING_ALPHA;
 
@@ -137,7 +153,6 @@ class UsermodBattery : public Usermod
           if (pinManager.allocatePin(batteryPin, false, PinOwner::UM_Battery)) {
             DEBUG_PRINTLN(F("Battery pin allocation succeeded."));
             success = true;
-            bat->setVoltage(readVoltage());
           }
 
         if (!success) {
@@ -148,10 +163,10 @@ class UsermodBattery : public Usermod
         }
       #else //ESP8266 boards have only one analog input pin A0
         pinMode(batteryPin, INPUT);
-        bat->setVoltage(readVoltage());
       #endif
 
-      nextReadTime = millis() + readingInterval;
+      // Delay the first voltage reading to allow voltage stabilization after powering up
+      nextReadTime = millis() + initialDelay;
       lastReadTime = millis();
 
       initDone = true;
@@ -177,6 +192,25 @@ class UsermodBattery : public Usermod
       if(strip.isUpdating()) return;
 
       lowPowerIndicator();
+
+      // Handling the initial delay
+      if (!initialDelayComplete && millis() < nextReadTime)
+        return; // Continue to return until the initial delay is over
+
+      // Once the initial delay is over, set it as complete
+      if (!initialDelayComplete)
+        {
+          initialDelayComplete = true;
+          // Set the regular interval after initial delay
+          nextReadTime = millis() + readingInterval;
+        }
+
+      // Make the first voltage reading once the initial delay has elapsed
+      if (isFirstReading)
+        {
+          voltage = readVoltage();
+          isFirstReading = false;
+        }
 
       // check the battery level every USERMOD_BATTERY_MEASUREMENT_INTERVAL (ms)
       if (millis() < nextReadTime) return;
