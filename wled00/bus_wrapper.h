118163d982a1cfa5a11652310d8ec5701bebb936
Troy <5659019+troyhacks@users.noreply.github.com>
2024-05-13 01:16:27 -0400

    Didn't work. Reverting.

--- wled00/bus_wrapper.h
+++ wled00/bus_wrapper.h
@@ -390,7 +390,7 @@ class PolyBus {
   };
   static void* create(uint8_t busType, uint8_t* pins, uint16_t len, uint8_t channel, uint16_t clock_kHz = 0U) {
     #if defined(ARDUINO_ARCH_ESP32) && !(defined(CONFIG_IDF_TARGET_ESP32S2) || defined(CONFIG_IDF_TARGET_ESP32S3) || defined(CONFIG_IDF_TARGET_ESP32C3))
-    #if defined(WLEDMM_FASTPATH) // && !defined(WLEDMM_SLOWPATH) // WLEDMM only for fastpath builds. TroyHacks: Removed the WLEDMM_SLOWPATH as it breaks WLEDMM_SLOWPATH
+    #if defined(WLEDMM_FASTPATH) && !defined(WLEDMM_SLOWPATH) // WLEDMM only for fastpath builds.
     // NOTE: "channel" is only used on ESP32 (and its variants) for RMT channel allocation
     // since 0.15.0-b3 I2S1 is favoured for classic ESP32 and moved to position 0 (channel 0) so we need to subtract 1 for correct RMT allocation
     if (channel > 0) channel--; // accommodate I2S1 which is used as 1st bus on classic ESP32
