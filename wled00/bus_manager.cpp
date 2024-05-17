a0514bb7ee66a41d00bd94ff908ac8a1b2ed553f
Frank <91616163+softhack007@users.noreply.github.com>
2024-05-17 21:40:59 +0200

    fix compiler warning

--- wled00/bus_manager.cpp
+++ wled00/bus_manager.cpp
@@ -41,10 +41,10 @@ uint8_t realtimeBroadcast(uint8_t type, IPAddress client, uint16_t length, byte 
 #endif
 #else
  // un-define USER_PRINT macros from bus_wrapper.h
- #undef DEBUG_PRINT
- #undef DEBUG_PRINTF
- #undef DEBUG_PRINTLN
- #undef DEBUG_FLUSH
+ #undef USER_PRINT
+ #undef USER_PRINTF
+ #undef USER_PRINTLN
+ #undef USER_FLUSH
  // WLEDMM use wled.h
 #include "wled.h"
 #endif
