16ae80a10ee1ace91f36198923644e9ab028c7af
devilpro1 <92226222+DevilPro1@users.noreply.github.com>
2024-05-26 00:38:41 +0200

    Update usermods_list.cpp

--- wled00/usermods_list.cpp
+++ wled00/usermods_list.cpp
@@ -9,7 +9,7 @@
  * || || ||
  * \/ \/ \/
  */
-//#include "../usermods/EXAMPLE_v2/usermod_v2_example.h"
+#include "../usermods/Autobrightness_with_photoresistor/usermod_autobrightness.h"
 
 #ifdef USERMOD_BATTERY
   #include "../usermods/Battery/usermod_v2_Battery.h"
@@ -211,7 +211,8 @@ void registerUsermods()
    * || || ||
    * \/ \/ \/
    */
-  //usermods.add(new MyExampleUsermod());
+  usermods.add(new UsermodAutobrightness());
+  
   #ifdef USERMOD_BATTERY
   usermods.add(new UsermodBattery());
   #endif
