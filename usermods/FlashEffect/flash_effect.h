e99be246223b264c155f0d7ebc9eaa45269c9683
Billy Donahue <billy.donahue@gmail.com>
2024-05-27 14:07:48 -0400

    aux0 to hold start time

--- usermods/FlashEffect/flash_effect.h
+++ usermods/FlashEffect/flash_effect.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "wled.h"
+#include "../../wled00/wled.h"
 
 /**
 platformio_override.ini example to enable this mod:
@@ -14,20 +14,34 @@ platformio_override.ini example to enable this mod:
     build_flags = ${env:esp32s3dev_8MB.build_flags} -DUSERMOD_FLASH_EFFECT
 */
 
-const char uiSpec_[] PROGMEM = "Flash@Duration;!,!;!;01";
-
 struct FlashEffect : Usermod {
   void setup() override {
-    strip.addEffect(255, &flashEffect_, uiSpec_);
+    static const char spec[] PROGMEM = "Flash@Duration;!,!;!;01";
+    strip.addEffect(255, &flashEffect_, spec);
   }
 
   void loop() override {}
 
+  /**
+    * onStateChanged() is used to detect WLED state change
+    * @mode parameter is CALL_MODE_... parameter used for notifications
+    */
+  void onStateChange(uint8_t mode) override {
+    Usermod::onStateChange(mode);
+    // do something if WLED state changed (color, brightness, effect, preset, etc)
+  }
+
 private:
   static uint16_t flashEffect_() {
+    // Use 'aux0' to store effect start time.
+    if (SEGMENT.call == 0)
+      SEGMENT.aux0 = strip.now;
     uint32_t maxDuration = 1000;
     uint32_t duration = SEGMENT.speed * maxDuration / 255 + FRAMETIME * 2;
-    SEGMENT.fill(strip.now < duration ? SEGCOLOR(0) : SEGCOLOR(1));
+    uint32_t color = SEGCOLOR(1);
+    if (strip.now - SEGMENT.aux0 < duration)
+      color = SEGCOLOR(0);
+    SEGMENT.fill(color);
     return FRAMETIME;
   }
 };
