578c6bb9d899dbf21bcd8e0e0207d42ed1a52891
Frank <91616163+softhack007@users.noreply.github.com>
2024-06-05 23:05:34 +0200

    make sure that HUB75 is recognized as "adressable leds"
    when IS_DIGITAL and IS_PWM are false, palettes and color wheel are not shown in UI

--- wled00/const.h
+++ wled00/const.h
@@ -253,7 +253,7 @@
 #define TYPE_NET_ARTNET_RGB      82            //network ArtNet RGB bus (master broadcast bus, unused)
 #define TYPE_NET_DDP_RGBW        88            //network DDP RGBW bus (master broadcast bus)
 
-#define IS_DIGITAL(t) ((t) & 0x10) //digital are 16-31 and 48-63
+#define IS_DIGITAL(t) (((t) & 0x10) || ((t)==TYPE_HUB75MATRIX)) //digital are 16-31 and 48-63 // WLEDMM added HUB75
 #define IS_PWM(t)     ((t) > 40 && (t) < 46)
 #define NUM_PWM_PINS(t) ((t) - 40) //for analog PWM 41-45 only
 #define IS_2PIN(t)      ((t) > 47)
