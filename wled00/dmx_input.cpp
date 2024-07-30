9a6e91d3e5039f252b347c314a2a69526c8cc34a
Will Tatam <will@netmindz.net>
2024-07-30 19:15:08 +0100

    DMX Input - reinstate loggers for connection state change

--- wled00/dmx_input.cpp
+++ wled00/dmx_input.cpp
@@ -186,6 +186,9 @@ void DMXInput::updateInternal()
   unsigned long now = millis();
   if (dmx_receive(inputPortNum, &packet, DMX_TIMEOUT_TICK)) {
     if (!packet.err) {
+      if(!connected) {
+        USER_PRINTLN("DMX Input - connected");
+      }
       connected = true;
       identify = isIdentifyOn();
       if (!packet.is_rdm) {
@@ -198,6 +201,9 @@ void DMXInput::updateInternal()
     }
   }
   else {
+    if(connected) {
+      USER_PRINTLN("DMX Input - disconnected");
+    }
     connected = false;
   }
 }
