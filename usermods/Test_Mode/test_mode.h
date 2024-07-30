b6be5fbf6b933e94867cc53d06c564e6ac0708b6
PaoloTK <github@paolotk.com>
2024-07-30 10:48:35 +0200

    removed inlining, testing changing color as well

--- usermods/Test_Mode/test_mode.h
+++ usermods/Test_Mode/test_mode.h
@@ -14,8 +14,6 @@
   #define TEST_EFFECT FX_MODE_FADE
 #endif
 
-// TODO: Move away from inlining all functions
-
 class TestModeUsermod : public Usermod {
   private:
     bool enabled = true;
@@ -23,107 +21,108 @@ class TestModeUsermod : public Usermod {
     unsigned long lastTime = 0;
     int8_t testButtons[WLED_MAX_BUTTONS];
     int8_t heldButtons = 0;
-    bool active = false;
+    bool testModeActivated = false;
 
   public:
     // strings to reduce flash memory usage (used more than twice)
     static const char _name[];
     static const char _enabled[];
+
     inline void enable(bool enable) { enabled = enable; }
     inline bool isEnabled() { return enabled; }
-    void setup() {
-      initDone = true;
-    }
 
-    void loop() {
-      if (!enabled) { return; }
+    void setup() { initDone = true; }
+    void loop();
 
-      if (millis() - lastTime > 1000) {
-        lastTime = millis();
-        heldButtons = 0;
-        for (unsigned i=0; i<WLED_MAX_BUTTONS; i++) {
-          if (testButtons[i] == 1) {
-            heldButtons += 1;
-          }
-        }
-        if (TEST_BUTTONS_NUMBER > 0 && heldButtons >= TEST_BUTTONS_NUMBER) {
-          DEBUG_PRINT(F("Test Mode activated"));
-          active = true;
-          bri = TEST_BRIGHTNESS;
-          stateUpdated(CALL_MODE_DIRECT_CHANGE);
-          effectCurrent = TEST_EFFECT;
-          colorUpdated(CALL_MODE_DIRECT_CHANGE);
-          enable(false);
-          serializeConfig();
-        }
-      
-      }
-
-      return;
-    }
-
-    bool handleButton(uint8_t b) override {
-      yield();
-      // once test mode has been activated keep handling buttons for this boot
-      if (!active) {
-        if (!enabled
-        // ignore certain button types as they may have other consequences
-        || buttonType[b] == BTN_TYPE_NONE
-        || buttonType[b] == BTN_TYPE_RESERVED
-        || buttonType[b] == BTN_TYPE_PIR_SENSOR
-        || buttonType[b] == BTN_TYPE_ANALOG
-        || buttonType[b] == BTN_TYPE_ANALOG_INVERTED) {
-          return false;
-        }
-      }
-
-      bool handled = false;
-      // do your button handling here
-      unsigned long now = millis();
-
-      if (isButtonPressed(b)) { //pressed
-        if (!buttonPressedBefore[b]) buttonPressedTime[b] = now;
-        buttonPressedBefore[b] = true;
-
-        if (now - buttonPressedTime[b] > 600) {
-            testButtons[b] = 1;
-            // DEBUG_PRINT(F("Button held: ")); DEBUG_PRINTLN(b);
-        }
-        buttonLongPressed[b] = true;
-      }
-      else if (!isButtonPressed(b) && buttonPressedBefore[b]) {
-        testButtons[b] = 0;
-        // DEBUG_PRINT(F("Button released: ")); DEBUG_PRINTLN(b);
-        buttonPressedBefore[b] = false;
-        buttonLongPressed[b] = false;
-      }
-
-      handled = true;
-      return handled;
-    }
-
-    void addToConfig(JsonObject& root) override
-    {
-      JsonObject top = root.createNestedObject(FPSTR(_name));
-      top[FPSTR(_enabled)] = enabled;
-    }
-
-    bool readFromConfig(JsonObject& root) override
-    {
-      JsonObject top = root[FPSTR(_name)];
-
-      bool configComplete = !top.isNull();
-
-      configComplete &= getJsonValue(top[FPSTR(_enabled)], enabled);
-
-      return configComplete;
-    }
-
-    uint16_t getId() override
-    {
-      return USERMOD_ID_TEST_MODE;
-    }
+    bool handleButton(uint8_t b) override;
+    void addToConfig(JsonObject& root) override;
+    bool readFromConfig(JsonObject& root) override;
+    uint16_t getId() override;
 };
+
+void TestModeUsermod::loop() {
+  if (!enabled || TEST_BUTTONS_NUMBER < 1) { return; }
+
+  if (millis() - lastTime > 1000) {
+    lastTime = millis();
+    heldButtons = 0;
+    for (unsigned i=0; i<WLED_MAX_BUTTONS; i++) {
+      if (testButtons[i] == 1) {
+        heldButtons += 1;
+      }
+    }
+    if (heldButtons >= TEST_BUTTONS_NUMBER) {
+      DEBUG_PRINT(F("Test Mode activated"));
+      testModeActivated = true;
+
+      bri = TEST_BRIGHTNESS;
+      stateUpdated(CALL_MODE_DIRECT_CHANGE);
+      effectCurrent = TEST_EFFECT;
+      colorUpdated(CALL_MODE_FX_CHANGED);
+      strip.setColor(0, ULTRAWHITE);
+      colorUpdated(CALL_MODE_DIRECT_CHANGE);
+
+      enable(false);
+      serializeConfig();
+    }
+  }
+}
+
+bool TestModeUsermod::handleButton(uint8_t b) {
+  yield();
+  // once test mode has been activated keep handling buttons for this boot
+  if (!testModeActivated) {
+    if (!enabled
+    // ignore certain button types as they may have other consequences
+    || buttonType[b] == BTN_TYPE_NONE
+    || buttonType[b] == BTN_TYPE_RESERVED
+    || buttonType[b] == BTN_TYPE_PIR_SENSOR
+    || buttonType[b] == BTN_TYPE_ANALOG
+    || buttonType[b] == BTN_TYPE_ANALOG_INVERTED) {
+      return false;
+    }
+  }
+
+  bool handled = false;
+  // do your button handling here
+  unsigned long now = millis();
+
+  if (isButtonPressed(b)) { //pressed
+    if (!buttonPressedBefore[b]) buttonPressedTime[b] = now;
+    buttonPressedBefore[b] = true;
+    // Button held
+    if (now - buttonPressedTime[b] > 600) {
+        testButtons[b] = 1;
+    }
+    buttonLongPressed[b] = true;
+  }
+  // Button released
+  else if (!isButtonPressed(b) && buttonPressedBefore[b]) {
+    testButtons[b] = 0;
+    buttonPressedBefore[b] = false;
+    buttonLongPressed[b] = false;
+  }
+
+  handled = true;
+  return handled;
+}
+
+void TestModeUsermod::addToConfig(JsonObject& root) {
+  JsonObject top = root.createNestedObject(FPSTR(_name));
+  top[FPSTR(_enabled)] = enabled;
+}
+
+bool TestModeUsermod::readFromConfig(JsonObject& root) {
+  JsonObject top = root[FPSTR(_name)];
+
+  bool configComplete = !top.isNull();
+
+  configComplete &= getJsonValue(top[FPSTR(_enabled)], enabled);
+
+  return configComplete;
+}
+
+uint16_t TestModeUsermod::getId() { return USERMOD_ID_TEST_MODE; }
 
 // add more strings here to reduce flash memory usage
 const char TestModeUsermod::_name[]    PROGMEM = "Test Mode";
