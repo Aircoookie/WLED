9405f5cc4dff3a16987e9445ed95fd4e596f2e79
Dustin Utecht <13292481+UtechtDustin@users.noreply.github.com>
2024-09-25 15:06:42 +0200

    Added ETH support for the cod.m WLED LAN Controller

--- wled00/const.h
+++ wled00/const.h
@@ -367,7 +367,7 @@
 #define BTN_TYPE_TOUCH_SWITCH     9
 
 //Ethernet board types
-#define WLED_NUM_ETH_TYPES        13
+#define WLED_NUM_ETH_TYPES        14
 
 #define WLED_ETH_NONE              0
 #define WLED_ETH_WT32_ETH01        1
@@ -382,6 +382,7 @@
 #define WLED_ETH_SERG74           10
 #define WLED_ETH_ESP32_POE_WROVER 11
 #define WLED_ETH_LILYGO_T_POE_PRO 12
+#define WLED_ETH_CODM             13
 
 //Hue error codes
 #define HUE_ERROR_INACTIVE        0
