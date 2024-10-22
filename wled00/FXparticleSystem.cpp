3f3f801b57ce6a73f8fd079a69f84f4dccb6cccf
Damian Schneider <daedae@gmx.ch>
2024-10-22 04:53:05 +0200

    Re-license from MIT to EUPL

--- wled00/FXparticleSystem.cpp
+++ wled00/FXparticleSystem.cpp
@@ -4,30 +4,8 @@
   Particle system with functions for particle generation, particle movement and particle rendering to RGB matrix.
   by DedeHai (Damian Schneider) 2013-2024
 
-  LICENSE
-  The MIT License (MIT)
-  Copyright (c) 2024 Damian Schneider
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-*/
-
-/*
-  Note on ESP32: using 32bit integer is faster than 16bit or 8bit, each operation takes on less instruction, can be testen on https://godbolt.org/
-  it does not matter if using int, unsigned int, uint32_t or int32_t, the compiler will make int into 32bit
-  this should be used to optimize speed but not if memory is affected much
+  Copyright (c) 2024  Damian Schneider
+  Licensed under the EUPL v. 1.2 or later
 */
 
 /*
--- wled00/FXparticleSystem.h
+++ wled00/FXparticleSystem.h
@@ -4,24 +4,8 @@
   Particle system with functions for particle generation, particle movement and particle rendering to RGB matrix.
   by DedeHai (Damian Schneider) 2013-2024
 
-  LICENSE
-  The MIT License (MIT)
-  Copyright (c) 2024  Damian Schneider 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
+  Copyright (c) 2024  Damian Schneider
+  Licensed under the EUPL v. 1.2 or later
 */
 
 #if !defined(WLED_DISABLE_PARTICLESYSTEM2D) || !defined(WLED_DISABLE_PARTICLESYSTEM1D)  
