2e01fe0b5bf853f068fc255896994b324a56c2d8
Will Miles <will@willmiles.net>
2024-10-23 21:34:35 -0400

    Revert "Replace SET_F with F in usermods"
    This reverts commit 7d067d8c305c3c6f397b6863f6849659a0aadd24.

--- usermods/Internal_Temperature_v2/usermod_internal_temperature.h
+++ usermods/Internal_Temperature_v2/usermod_internal_temperature.h
@@ -149,11 +149,11 @@ public:
     void appendConfigData()
     {
     // Display 'ms' next to the 'Loop Interval' setting
-    oappend(F("addInfo('Internal Temperature:Loop Interval', 1, 'ms');"));
+    oappend(SET_F("addInfo('Internal Temperature:Loop Interval', 1, 'ms');"));
     // Display '°C' next to the 'Activation Threshold' setting
-    oappend(F("addInfo('Internal Temperature:Activation Threshold', 1, '°C');"));
+    oappend(SET_F("addInfo('Internal Temperature:Activation Threshold', 1, '°C');"));
     // Display '0 = Disabled' next to the 'Preset To Activate' setting
-    oappend(F("addInfo('Internal Temperature:Preset To Activate', 1, '0 = unused');"));
+    oappend(SET_F("addInfo('Internal Temperature:Preset To Activate', 1, '0 = unused');"));
     }
 
   bool readFromConfig(JsonObject &root)
