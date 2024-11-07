896576f4df9de1172b738c7a23eba5cdda2fd6cc
maxi4329 <maxistrasser08@gmail.com>
2024-11-07 13:41:17 +0100

    undid more formating changes

--- wled00/data/common.js
+++ wled00/data/common.js
@@ -40,12 +40,12 @@ function showTooltip(element) {
 }
 
 function hideTooltip(element) {
-	d.querySelectorAll('.tooltip').forEach((tooltip)=>{
-		tooltip.classList.remove("visible");
-		d.body.removeChild(tooltip);
-	});
-	// restore title
-	element.setAttribute("title", element.getAttribute("data-title"));
+			d.querySelectorAll('.tooltip').forEach((tooltip)=>{
+				tooltip.classList.remove("visible");
+				d.body.removeChild(tooltip);
+			});
+			// restore title
+			element.setAttribute("title", element.getAttribute("data-title"));
 }
 
 function tooltip(cont = null) {
