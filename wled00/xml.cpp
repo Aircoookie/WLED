49fb16e2c6528ddc98cccc352cc8f0d4a5f62a13
Wouter Gritter <wouter@gritter.me>
2024-11-21 10:52:22 +0100

    Introduce printSetInputMaxlength to properly set an inputs maxlength variable

--- wled00/xml.cpp
+++ wled00/xml.cpp
@@ -465,8 +465,9 @@ void getSettingsJS(byte subPage, Print& settingsScript)
     printSetFormValue(settingsScript,PSTR("MG"),mqttGroupTopic);
     printSetFormCheckbox(settingsScript,PSTR("BM"),buttonPublishMqtt);
     printSetFormCheckbox(settingsScript,PSTR("RT"),retainMqttMsg);
-    settingsScript.printf_P(PSTR("d.Sf.MD.maxlength=%d;d.Sf.MG.maxlength=%d;d.Sf.MS.maxlength=%d;"),
-                  MQTT_MAX_TOPIC_LEN, MQTT_MAX_TOPIC_LEN, MQTT_MAX_SERVER_LEN);
+    printSetInputMaxLength(settingsScript, PSTR("MD"), MQTT_MAX_TOPIC_LEN);
+    printSetInputMaxLength(settingsScript, PSTR("MG"), MQTT_MAX_TOPIC_LEN);
+    printSetInputMaxLength(settingsScript, PSTR("MS"), MQTT_MAX_SERVER_LEN);
     #else
     settingsScript.print(F("toggle('MQTT');"));    // hide MQTT settings
     #endif
