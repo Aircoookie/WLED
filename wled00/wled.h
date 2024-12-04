e8d9891d13baba4cb237f0e83c69f26c7ffa33b1
Will Tatam <will@netmindz.net>
2024-12-04 18:57:32 +0000

    set version to dev

--- wled00/wled.h
+++ wled00/wled.h
@@ -3,12 +3,12 @@
 /*
    Main sketch, global variable declarations
    @title WLED project sketch
-   @version 0.15.0-b7
+   @version 0.15.0-dev
    @author Christian Schwinne
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2410270
+#define VERSION 2412040
 
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
