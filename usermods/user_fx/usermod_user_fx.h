4218da5e5d731a46dbd158750de7356ed7312df8
Mariusz Ryndzionek <mryndzionek@gmail.com>
2025-01-15 09:45:08 +0100

    Further code review adjustments:
    - use hw_random8 instead of random8
     - changed diffusion calculation to fixed point math
     - adjusted general code formatting

--- usermods/user_fx/usermod_user_fx.h
+++ usermods/user_fx/usermod_user_fx.h
@@ -1,15 +1,13 @@
-static uint16_t mode_static(void)
-{
+static uint16_t mode_static(void) {
   SEGMENT.fill(SEGCOLOR(0));
   return strip.isOffRefreshRequired() ? FRAMETIME : 350;
 }
 
-static uint16_t mode_diffusionfire(void)
-{
+static uint16_t mode_diffusionfire(void) {
   static uint32_t call = 0;
 
   if (!strip.isMatrix || !SEGMENT.is2D())
-    return mode_static(); // not a 2D set-up
+    return mode_static();  // not a 2D set-up
 
   const int cols = SEG_W;
   const int rows = SEG_H;
@@ -22,38 +20,32 @@ static uint16_t mode_diffusionfire(void)
 
   unsigned dataSize = SEGMENT.length() + (cols * sizeof(uint16_t));
   if (!SEGENV.allocateData(dataSize))
-    return mode_static(); // allocation failed
+    return mode_static();  // allocation failed
 
-  if (SEGENV.call == 0)
-  {
+  if (SEGENV.call == 0) {
     SEGMENT.fill(BLACK);
     SEGENV.step = 0;
     call = 0;
   }
 
-  if ((strip.now - SEGENV.step) >= refresh_ms)
-  {
+  if ((strip.now - SEGENV.step) >= refresh_ms) {
     uint16_t *tmp_row = (uint16_t *)(SEGMENT.data + SEGMENT.length());
     SEGENV.step = strip.now;
     call++;
 
     // scroll up
     for (unsigned y = 1; y < rows; y++)
-      for (unsigned x = 0; x < cols; x++)
-      {
+      for (unsigned x = 0; x < cols; x++) {
         unsigned src = SEGMENT.XY(x, y);
         unsigned dst = SEGMENT.XY(x, y - 1);
         SEGMENT.data[dst] = SEGMENT.data[src];
       }
 
-    if (random8() > turbulence)
-    {
+    if (hw_random8() > turbulence) {
       // create new sparks at bottom row
-      for (unsigned x = 0; x < cols; x++)
-      {
-        uint8_t p = random8();
-        if (p < spark_rate)
-        {
+      for (unsigned x = 0; x < cols; x++) {
+        uint8_t p = hw_random8();
+        if (p < spark_rate) {
           unsigned dst = SEGMENT.XY(x, rows - 1);
           SEGMENT.data[dst] = 255;
         }
@@ -61,32 +53,24 @@ static uint16_t mode_diffusionfire(void)
     }
 
     // diffuse
-    for (unsigned y = 0; y < rows; y++)
-    {
-      for (unsigned x = 0; x < cols; x++)
-      {
+    for (unsigned y = 0; y < rows; y++) {
+      for (unsigned x = 0; x < cols; x++) {
         unsigned v = SEGMENT.data[SEGMENT.XY(x, y)];
-        if (x > 0)
-        {
+        if (x > 0) {
           v += SEGMENT.data[SEGMENT.XY(x - 1, y)];
         }
-        if (x < (cols - 1))
-        {
+        if (x < (cols - 1)) {
           v += SEGMENT.data[SEGMENT.XY(x + 1, y)];
         }
-        tmp_row[x] = min(255, (int)(v / (3.0 + (float)diffusion / 100.0)));
+        tmp_row[x] = min(255, (int)(v * 100 / (300 + diffusion)));
       }
 
-      for (unsigned x = 0; x < cols; x++)
-      {
+      for (unsigned x = 0; x < cols; x++) {
         SEGMENT.data[SEGMENT.XY(x, y)] = tmp_row[x];
-        if (SEGMENT.check1)
-        {
+        if (SEGMENT.check1) {
           CRGB color = ColorFromPalette(SEGPALETTE, tmp_row[x], 255, NOBLEND);
           SEGMENT.setPixelColorXY(x, y, color);
-        }
-        else
-        {
+        } else {
           uint32_t color = SEGCOLOR(0);
           SEGMENT.setPixelColorXY(x, y, color_fade(color, tmp_row[x]));
         }
@@ -95,24 +79,18 @@ static uint16_t mode_diffusionfire(void)
   }
   return FRAMETIME;
 }
-static const char _data_FX_MODE_DIFFUSIONFIRE[] PROGMEM = "Diffusion Fire@!,Spark rate,Diffusion Speed,Turbulence,,Use palette;;Color;;2;pal=35";
+static const char _data_FX_MODE_DIFFUSIONFIRE[] PROGMEM =
+    "Diffusion Fire@!,Spark rate,Diffusion Speed,Turbulence,,Use "
+    "palette;;Color;;2;pal=35";
 
-class UserFxUsermod : public Usermod
-{
-
-private:
-public:
-  void setup()
-  {
+class UserFxUsermod : public Usermod {
+ private:
+ public:
+  void setup() {
     strip.addEffect(255, &mode_diffusionfire, _data_FX_MODE_DIFFUSIONFIRE);
   }
 
-  void loop()
-  {
-  }
+  void loop() {}
 
-  uint16_t getId()
-  {
-    return USERMOD_ID_USER_FX;
-  }
+  uint16_t getId() { return USERMOD_ID_USER_FX; }
 };
